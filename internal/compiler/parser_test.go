3219ca4946004030ec745bee3ded004a1fd9fd7f
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2024-10-23 07:08:34 -0700

    Baselines are 99% identical.
    I think the remaining diffs are bugs or else the
    BindingElement/OmittedExpression difference.

--- internal/compiler/parser_test.go
+++ internal/compiler/parser_test.go
@@ -52,21 +52,25 @@ func printAST(t *testing.T, sourceRoot string, targetRoot string, sourceFile *So
 		t.Errorf("Error creating file %s: %v\n", outputFileName, err)
 	}
 	defer file.Close()
-	// Recursive function to visit nodes with indentation
-	var v func(node *Node, indentation int) bool
-	v = func(node *Node, indentation int) bool {
-		indent := strings.Repeat("  ", indentation)
-		if node.kind == SyntaxKindIdentifier {
-			fmt.Fprintf(file, "%s%s: '%s'\n", indent, node.kind, sourceFile.text[node.loc.pos:node.loc.end])
-		} else {
-			fmt.Fprintf(file, "%s%s\n", indent, node.kind)
+	var visit func(node *Node, indentation int) bool
+	visit = func(node *Node, indentation int) bool {
+		offset := 1
+		skind := node.kind.String()[len("SyntaxKind"):]
+		switch node.kind {
+		case SyntaxKindModifierList, SyntaxKindTypeParameterList, SyntaxKindTypeArgumentList, SyntaxKindSyntaxList:
+			offset = 0
+		case SyntaxKindIdentifier:
+			indent := strings.Repeat("  ", indentation)
+			fmt.Fprintf(file, "%s%s: '%s'\n", indent, skind, sourceFile.text[node.loc.pos:node.loc.end])
+		default:
+			indent := strings.Repeat("  ", indentation)
+			fmt.Fprintf(file, "%s%s\n", indent, skind)
 		}
-		// TODO: Include trivia
-		// Visit child nodes with increased indentation
+		// TODO: Include trivia in a more structured way than GetFullText// Visit child nodes with increased indentation
 		return node.ForEachChild(func(child *Node) bool {
-			v(child, indentation+1)
+			visit(child, indentation+offset)
 			return false
 		})
 	}
-	sourceFile.ForEachChild(func(n *Node) bool { return v(n, 0) })
+	visit(sourceFile.AsNode(), 0)
 }
