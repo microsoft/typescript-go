3fa53557bab9e4cf3828cb39f6dd2b6c3163e0bf
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-10-28 14:15:21 -0700

    Move fixture code

--- internal/compiler/fixtures_test.go
+++ internal/compiler/fixtures_test.go
@@ -4,13 +4,13 @@ import (
 	"path/filepath"
 
 	"github.com/microsoft/typescript-go/internal/repo"
-	"github.com/microsoft/typescript-go/internal/testutil"
+	"github.com/microsoft/typescript-go/internal/testutil/filefixture"
 )
 
-var benchFixtures = []testutil.FileFixture{
-	testutil.NewFileFixtureFromString("empty.ts", "empty.ts", ""),
-	testutil.NewFileFixtureFromFile("checker.ts", filepath.Join(repo.TypeScriptSubmodulePath, "src/compiler/checker.ts")),
-	testutil.NewFileFixtureFromFile("dom.generated.d.ts", filepath.Join(repo.TypeScriptSubmodulePath, "src/lib/dom.generated.d.ts")),
-	testutil.NewFileFixtureFromFile("Herebyfile.mjs", filepath.Join(repo.TypeScriptSubmodulePath, "Herebyfile.mjs")),
-	testutil.NewFileFixtureFromFile("jsxComplexSignatureHasApplicabilityError.tsx", filepath.Join(repo.TypeScriptSubmodulePath, "tests/cases/compiler/jsxComplexSignatureHasApplicabilityError.tsx")),
+var benchFixtures = []filefixture.Fixture{
+	filefixture.FromString("empty.ts", "empty.ts", ""),
+	filefixture.FromFile("checker.ts", filepath.Join(repo.TypeScriptSubmodulePath, "src/compiler/checker.ts")),
+	filefixture.FromFile("dom.generated.d.ts", filepath.Join(repo.TypeScriptSubmodulePath, "src/lib/dom.generated.d.ts")),
+	filefixture.FromFile("Herebyfile.mjs", filepath.Join(repo.TypeScriptSubmodulePath, "Herebyfile.mjs")),
+	filefixture.FromFile("jsxComplexSignatureHasApplicabilityError.tsx", filepath.Join(repo.TypeScriptSubmodulePath, "tests/cases/compiler/jsxComplexSignatureHasApplicabilityError.tsx")),
 }
