37e8851dda2642046f2688c7dd66d9638710a5b6
Daniel Rosenwasser <DanielRosenwasser@users.noreply.github.com>
2024-11-05 19:16:29 +0000

    Switch to top-level `MustCompile`.

--- internal/compiler/semver.go
+++ internal/compiler/semver.go
@@ -2,6 +2,7 @@ package compiler
 
 import (
 	"cmp"
+	"regexp"
 	"slices"
 	"strconv"
 	"strings"
@@ -13,26 +14,26 @@ import (
 //
 // NOTE: We differ here in that we allow X and X.Y, with missing parts having the default
 // value of `0`.
-var versionRegexp = makeRegexp("^(0|[1-9]\\d*)(?:\\.(0|[1-9]\\d*)(?:\\.(0|[1-9]\\d*)(?:-([a-zA-Z0-9-.]+))?(?:\\+([a-zA-Z0-9-.]+))?)?)?$")
+var versionRegexp = regexp.MustCompile("^(0|[1-9]\\d*)(?:\\.(0|[1-9]\\d*)(?:\\.(0|[1-9]\\d*)(?:-([a-zA-Z0-9-.]+))?(?:\\+([a-zA-Z0-9-.]+))?)?)?$")
 
 // https://semver.org/#spec-item-9
 // > A pre-release version MAY be denoted by appending a hyphen and a series of dot separated
 // > identifiers immediately following the patch version. Identifiers MUST comprise only ASCII
 // > alphanumerics and hyphen [0-9A-Za-z-]. Identifiers MUST NOT be empty. Numeric identifiers
 // > MUST NOT include leading zeroes.
-var prereleaseRegexp = makeRegexp("^(?:0|[1-9]\\d*|[a-zA-Z-][a-zA-Z0-9-]*)(?:\\.(?:0|[1-9]\\d*|[a-zA-Z-][a-zA-Z0-9-]*))*$")
-var prereleasePartRegexp = makeRegexp("^(?:0|[1-9]\\d*|[a-zA-Z-][a-zA-Z0-9-]*)$")
+var prereleaseRegexp = regexp.MustCompile("^(?:0|[1-9]\\d*|[a-zA-Z-][a-zA-Z0-9-]*)(?:\\.(?:0|[1-9]\\d*|[a-zA-Z-][a-zA-Z0-9-]*))*$")
+var prereleasePartRegexp = regexp.MustCompile("^(?:0|[1-9]\\d*|[a-zA-Z-][a-zA-Z0-9-]*)$")
 
 // https://semver.org/#spec-item-10
 // > Build metadata MAY be denoted by appending a plus sign and a series of dot separated
 // > identifiers immediately following the patch or pre-release version. Identifiers MUST
 // > comprise only ASCII alphanumerics and hyphen [0-9A-Za-z-]. Identifiers MUST NOT be empty.
-var buildRegExp = makeRegexp("^[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$")
-var buildPartRegExp = makeRegexp("^[a-zA-Z0-9-]+$")
+var buildRegExp = regexp.MustCompile("^[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$")
+var buildPartRegExp = regexp.MustCompile("^[a-zA-Z0-9-]+$")
 
 // https://semver.org/#spec-item-9
 // > Numeric identifiers MUST NOT include leading zeroes.
-var numericIdentifierRegExp = makeRegexp("^(?:0|[1-9]\\d*)$")
+var numericIdentifierRegExp = regexp.MustCompile("^(?:0|[1-9]\\d*)$")
 
 type Version struct {
 	// !!!
@@ -191,8 +192,8 @@ type VersionRange struct {
 }
 
 type versionComparator struct {
-	operand  Version
 	operator comparatorOperator
+	operand  Version
 }
 
 type comparatorOperator int
@@ -210,8 +211,8 @@ const (
 // range-set    ::= range ( logical-or range ) *
 // range        ::= hyphen | simple ( ' ' simple ) * | ”
 // logical-or   ::= ( ' ' ) * '||' ( ' ' ) *
-var logicalOrRegExp = makeRegexp("\\|\\|")
-var whitespaceRegExp = makeRegexp("\\s+")
+var logicalOrRegExp = regexp.MustCompile("\\|\\|")
+var whitespaceRegExp = regexp.MustCompile("\\s+")
 
 // https://github.com/npm/node-semver#range-grammar
 //
@@ -223,4 +224,17 @@ var whitespaceRegExp = makeRegexp("\\s+")
 // build        ::= parts
 // parts        ::= part ( '.' part ) *
 // part         ::= nr | [-0-9A-Za-z]+
-var partialRegExp = makeRegexp("^([x*0]|[1-9]\\d*)(?:\\.([x*0]|[1-9]\\d*)(?:\\.([x*0]|[1-9]\\d*)(?:-([a-zA-Z0-9-.]+))?(?:\\+([a-zA-Z0-9-.]+))?)?)?$")
+var partialRegExp = regexp.MustCompile("^([x*0]|[1-9]\\d*)(?:\\.([x*0]|[1-9]\\d*)(?:\\.([x*0]|[1-9]\\d*)(?:-([a-zA-Z0-9-.]+))?(?:\\+([a-zA-Z0-9-.]+))?)?)?$")
+
+// https://github.com/npm/node-semver#range-grammar
+//
+// hyphen       ::= partial ' - ' partial
+var hyphenRegExp = regexp.MustCompile("^\\s*([a-zA-Z0-9-+.*]+)\\s+-\\s+([a-zA-Z0-9-+.*]+)\\s*$")
+
+// https://github.com/npm/node-semver#range-grammar
+//
+// simple       ::= primitive | partial | tilde | caret
+// primitive    ::= ( '<' | '>' | '>=' | '<=' | '=' ) partial
+// tilde        ::= '~' partial
+// caret        ::= '^' partial
+var rangeRegExp = regexp.MustCompile("^([~^<>=]|<=|>=)?\\s*([a-zA-Z0-9-+.*]+)$")
