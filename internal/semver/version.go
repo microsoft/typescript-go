efe2d7473ce682571d6f539bac82a6274b62b4d0
Daniel Rosenwasser <DanielRosenwasser@users.noreply.github.com>
2024-11-06 23:16:48 +0000

    Don't need a separate file.

--- internal/semver/version.go
+++ internal/semver/version.go
@@ -3,8 +3,10 @@ package semver
 import (
 	"cmp"
 	"fmt"
+	"math"
 	"regexp"
 	"slices"
+	"strconv"
 	"strings"
 )
 
@@ -231,4 +233,25 @@ func TryParseVersion(text string) (Version, error) {
 	}
 
 	return result, nil
+}
+
+func stringToNumber(s string) float64 {
+	// !!! Copied from the core compiler.
+	// !!! This function should behave identically to the expression `+s` in JS
+	// This includes parsing binary, octal, and hex numeric strings
+	value, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		return math.NaN()
+	}
+	return value
+}
+
+func getUintComponent(text string) uint32 {
+	r, err := strconv.ParseUint(text, 10, 32)
+	if err != nil {
+		// !!!
+		// Should we actually just panic here?
+		panic(err.Error())
+	}
+	return uint32(r)
 }
