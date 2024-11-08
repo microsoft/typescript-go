c1fcec9615602f8d47ecacbd8b18e097fc4f91cd
Gabriela Araujo Britto <gabrielaa@microsoft.com>
2024-11-08 12:11:39 -0800

    fix bad merge

--- internal/compiler/utilities.go
+++ internal/compiler/utilities.go
@@ -2,7 +2,6 @@ package compiler
 
 import (
 	"fmt"
-	"iter"
 	"maps"
 	"math"
 	"path/filepath"
@@ -511,158 +510,6 @@ func formatMessage(message *diagnostics.Message, args ...any) string {
 	return text
 }
 
-func filter[T any](slice []T, predicate func(T) bool) []T {
-	result, _ := sameFilter(slice, predicate)
-	return result
-}
-
-func sameFilter[T any](slice []T, predicate func(T) bool) ([]T, bool) {
-	for i, value := range slice {
-		if !predicate(value) {
-			result := slice[0:i]
-			i++
-			for i < len(slice) {
-				value = slice[i]
-				if predicate(value) {
-					result = append(result, value)
-				}
-				i++
-			}
-			return result, false
-		}
-	}
-	return slice, true
-}
-
-func mapf[T, U any](slice []T, f func(T) U) []U {
-	if len(slice) == 0 {
-		return nil
-	}
-	result := make([]U, len(slice))
-	for i := range slice {
-		result[i] = f(slice[i])
-	}
-	return result
-}
-
-func mapIndex[T, U any](slice []T, f func(T, int) U) []U {
-	if len(slice) == 0 {
-		return nil
-	}
-	result := make([]U, len(slice))
-	for i := range slice {
-		result[i] = f(slice[i], i)
-	}
-	return result
-}
-
-func sameMap[T comparable](slice []T, f func(T) T) []T {
-	for i, value := range slice {
-		mapped := f(value)
-		if mapped != value {
-			result := make([]T, len(slice))
-			copy(result, slice[:i])
-			result[i] = mapped
-			for j := i + 1; j < len(slice); j++ {
-				result[j] = f(slice[j])
-			}
-			return result
-		}
-	}
-	return slice
-}
-
-func sameMapIndex[T comparable](slice []T, f func(T, int) T) ([]T, bool) {
-	for i, value := range slice {
-		mapped := f(value, i)
-		if mapped != value {
-			result := make([]T, len(slice))
-			copy(result, slice[:i])
-			result[i] = mapped
-			for i, value := range slice[i+1:] {
-				result[i] = f(value, i)
-			}
-			return result, false
-		}
-	}
-	return slice, true
-}
-
-func some[T any](array []T, predicate func(T) bool) bool {
-	for _, value := range array {
-		if predicate(value) {
-			return true
-		}
-	}
-	return false
-}
-
-func every[T any](array []T, predicate func(T) bool) bool {
-	for _, value := range array {
-		if !predicate(value) {
-			return false
-		}
-	}
-	return true
-}
-
-func insertSorted[T any](slice []T, element T, cmp func(T, T) int) []T {
-	i, _ := slices.BinarySearchFunc(slice, element, cmp)
-	return slices.Insert(slice, i, element)
-}
-
-func firstOrNil[T any](slice []T) T {
-	if len(slice) != 0 {
-		return slice[0]
-	}
-	return *new(T)
-}
-
-func firstOrNilSeq[T any](seq iter.Seq[T]) T {
-	if seq != nil {
-		for value := range seq {
-			return value
-		}
-	}
-	return *new(T)
-}
-
-func lastOrNil[T any](slice []T) T {
-	if len(slice) != 0 {
-		return slice[len(slice)-1]
-	}
-	return *new(T)
-}
-
-func find[T any](slice []T, predicate func(T) bool) T {
-	for _, value := range slice {
-		if predicate(value) {
-			return value
-		}
-	}
-	return *new(T)
-}
-
-func findLast[T any](slice []T, predicate func(T) bool) T {
-	for i := len(slice) - 1; i >= 0; i-- {
-		value := slice[i]
-		if predicate(value) {
-			return value
-		}
-	}
-	return *new(T)
-}
-
-func findLastIndex[T any](slice []T, predicate func(T) bool) int {
-	for i := len(slice) - 1; i >= 0; i-- {
-		value := slice[i]
-		if predicate(value) {
-			return i
-		}
-	}
-	return -1
-}
-
 func findInMap[K comparable, V any](m map[K]V, predicate func(V) bool) V {
 	for _, value := range m {
 		if predicate(value) {
@@ -670,39 +517,6 @@ func findInMap[K comparable, V any](m map[K]V, predicate func(V) bool) V {
 		}
 	}
 	return *new(V)
-}
-
-func concatenate[T any](s1 []T, s2 []T) []T {
-	if len(s2) == 0 {
-		return s1
-	}
-	if len(s1) == 0 {
-		return s2
-	}
-	return slices.Concat(s1, s2)
-}
-
-func countWhere[T any](slice []T, predicate func(T) bool) int {
-	count := 0
-	for _, value := range slice {
-		if predicate(value) {
-			count++
-		}
-	}
-	return count
-}
-
-func replaceElement[T any](slice []T, i int, t T) []T {
-	result := slices.Clone(slice)
-	result[i] = t
-	return result
-}
-
-func identical[T any](s1 []T, s2 []T) bool {
-	if len(s1) == len(s2) {
-		return len(s1) == 0 || &s1[0] == &s2[0]
-	}
-	return false
 }
 
 func boolToTristate(b bool) Tristate {
