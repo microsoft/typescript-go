c3d62ebdf438183d092fe5f6d2630e2a074ac4c0
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-11-15 11:24:54 -0800

    wip

--- internal/vfs/vfs.go
+++ internal/vfs/vfs.go
@@ -2,6 +2,11 @@ package vfs
 
 import (
 	"io/fs"
+	"os"
+	"runtime"
+	"strings"
+	"sync"
+	"unicode"
 
 	"github.com/microsoft/typescript-go/internal/tspath"
 )
@@ -21,12 +26,73 @@ type FS interface {
 
 var _ FS = (*adapter)(nil)
 
+func FromOS(cwd string) FS {
+	if cwd == "" {
+		panic("cwd must be provided")
+	}
+
+	useCaseSensitiveFileNames := isFileSystemCaseSensitive()
+	return &adapter{
+		cwd:                       tspath.ToPath(cwd, "", useCaseSensitiveFileNames),
+		useCaseSensitiveFileNames: useCaseSensitiveFileNames,
+		rootFor:                   os.DirFS,
+	}
+}
+
+// FromIOFS creates a new FS from an [fs.FS].
+// For paths like `c:/foo/bar`, fsys will be used as though the path is `/c:/foo/bar`.
+func FromIOFS(cwd string, useCaseSensitiveFileNames bool, fsys fs.FS) FS {
+	if cwd == "" {
+		panic("cwd must be provided")
+	}
+
+	return &adapter{
+		cwd:                       tspath.ToPath(cwd, "", useCaseSensitiveFileNames),
+		useCaseSensitiveFileNames: useCaseSensitiveFileNames,
+		rootFor: func(root string) fs.FS {
+			if root == "/" {
+				return fsys
+			}
+
+			sub, err := fs.Sub(fsys, tspath.RemoveTrailingDirectorySeparator(root))
+			if err != nil {
+				panic(err)
+			}
+			return sub
+		},
+	}
+}
+
+var isFileSystemCaseSensitive = sync.OnceValue(func() bool {
+	// win32/win64 are case insensitive platforms
+	if runtime.GOOS == "windows" {
+		return false
+	}
+
+	// If the current executable exists under a different case, we must be case-insensitve.
+	if _, err := os.Stat(swapCase(os.Args[0])); os.IsNotExist(err) {
+		return false
+	}
+	return true
+})
+
+// Convert all lowercase chars to uppercase, and vice-versa
+func swapCase(str string) string {
+	return strings.Map(func(r rune) rune {
+		upper := unicode.ToUpper(r)
+		if upper == r {
+			return unicode.ToLower(r)
+		} else {
+			return upper
+		}
+	}, str)
+}
+
 type adapter struct {
 	cwd                       tspath.Path
 	useCaseSensitiveFileNames bool
 
-	// Map from the "encoded root" to the FS that should be used for that root.
-	roots map[string]fs.FS
+	rootFor func(root string) fs.FS
 }
 
 func (a *adapter) UseCaseSensitiveFileNames() bool {
@@ -49,17 +115,17 @@ func splitPath(p tspath.Path) (rootName, rest string) {
 	return string(p[:l]), string(p[l:])
 }
 
-func (a *adapter) rootFor(path tspath.Path) (fs.FS, string) {
+func (a *adapter) rootAndPath(path tspath.Path) (fs.FS, string) {
 	rootName, rest := splitPath(path)
-	return a.roots[rootName], rest
+	return a.rootFor(rootName), rest
 }
 
 func (a *adapter) stat(path tspath.Path) fs.FileInfo {
-	root, rest := a.rootFor(path)
-	if root == nil {
+	fsys, rest := a.rootAndPath(path)
+	if fsys == nil {
 		return nil
 	}
-	stat, err := fs.Stat(root, rest)
+	stat, err := fs.Stat(fsys, rest)
 	if err != nil {
 		return nil
 	}
@@ -77,12 +143,12 @@ func (a *adapter) DirectoryExists(path tspath.Path) bool {
 }
 
 func (a *adapter) GetDirectories(path tspath.Path) []string {
-	root, rest := a.rootFor(path)
-	if root == nil {
+	fsys, rest := a.rootAndPath(path)
+	if fsys == nil {
 		return nil
 	}
 
-	entries, err := fs.ReadDir(root, rest)
+	entries, err := fs.ReadDir(fsys, rest)
 	if err != nil {
 		return nil
 	}
@@ -97,11 +163,11 @@ func (a *adapter) GetDirectories(path tspath.Path) []string {
 }
 
 func (a *adapter) ReadFile(path tspath.Path) string {
-	root, rest := a.rootFor(path)
-	if root == nil {
+	fsys, rest := a.rootAndPath(path)
+	if fsys == nil {
 		return ""
 	}
-	contents, err := fs.ReadFile(root, rest)
+	contents, err := fs.ReadFile(fsys, rest)
 	if err != nil {
 		return ""
 	}
