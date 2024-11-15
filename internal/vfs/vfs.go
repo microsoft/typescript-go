4ff70c658b4f5253d3e974fea827b7a5858fcc0a
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-11-15 13:25:29 -0800

    more

--- internal/vfs/vfs.go
+++ internal/vfs/vfs.go
@@ -1,31 +1,65 @@
 package vfs
 
 import (
+	"bytes"
+	"encoding/binary"
 	"io/fs"
 	"os"
 	"runtime"
 	"strings"
 	"sync"
 	"unicode"
+	"unicode/utf16"
 
 	"github.com/microsoft/typescript-go/internal/tspath"
 )
 
+// FS is a file system abstraction. All paths are handled as [tspath.Path].
 type FS interface {
+	// UseCaseSensitiveFileNames returns true if the file system is case-sensitive.
 	UseCaseSensitiveFileNames() bool
 
+	// GetCurrentDirectory returns the current directory.
 	GetCurrentDirectory() tspath.Path
+
+	// ToPath converts a string to a path. If the path is relative, it is resolved against the current directory.
 	ToPath(p string) tspath.Path
 
+	// FileExists returns true if the file exists.
 	FileExists(path tspath.Path) bool
-	ReadFile(path tspath.Path) string
 
+	// ReadFile reads the file specified by path and returns the content.
+	// If the file fails to be read, ok will be false.
+	ReadFile(path tspath.Path) (contents string, ok bool)
+
+	// DirectoryExists returns true if the path is a directory.
 	DirectoryExists(path tspath.Path) bool
+
+	// GetDirectories returns the names of the directories in the specified directory.
 	GetDirectories(path tspath.Path) []string
+
+	// WalkDir walks the file tree rooted at root, calling walkFn for each file or directory in the tree.
+	// It is has the same behavior as [fs.WalkDir], but with paths as [tspath.Path].
+	WalkDir(root tspath.Path, walkFn WalkDirFunc) error
 }
+
+// DirEntry is [fs.DirEntry].
+type DirEntry = fs.DirEntry
+
+// WalkDirFunc is [fs.WalkDirFunc] but with paths as [tspath.Path].
+type WalkDirFunc func(path tspath.Path, d DirEntry, err error) error
+
+var (
+	// SkipAll is [fs.SkipAll].
+	SkipAll = fs.SkipAll
+
+	// SkipDir is [fs.SkipDir].
+	SkipDir = fs.SkipDir
+)
 
 var _ FS = (*adapter)(nil)
 
+// FromOS creates a new FS from the OS file system.
 func FromOS(cwd string) FS {
 	if cwd == "" {
 		panic("cwd must be provided")
@@ -137,6 +171,42 @@ func (a *adapter) FileExists(path tspath.Path) bool {
 	return stat != nil && !stat.IsDir()
 }
 
+func (a *adapter) ReadFile(path tspath.Path) (contents string, ok bool) {
+	fsys, rest := a.rootAndPath(path)
+	if fsys == nil {
+		return "", false
+	}
+
+	b, err := fs.ReadFile(fsys, rest)
+	if err != nil {
+		return "", false
+	}
+
+	var bom [2]byte
+	if len(b) >= 2 {
+		bom = [2]byte{b[0], b[1]}
+		switch bom {
+		case [2]byte{0xFF, 0xFE}:
+			return decodeUtf16(b[2:], binary.LittleEndian), true
+		case [2]byte{0xFE, 0xFF}:
+			return decodeUtf16(b[2:], binary.BigEndian), true
+		}
+	}
+	if len(b) >= 3 && b[0] == 0xEF && b[1] == 0xBB && b[2] == 0xBF {
+		b = b[3:]
+	}
+
+	return string(b), true
+}
+
+func decodeUtf16(b []byte, order binary.ByteOrder) string {
+	ints := make([]uint16, len(b)/2)
+	if err := binary.Read(bytes.NewReader(b), order, &ints); err != nil {
+		return ""
+	}
+	return string(utf16.Decode(ints))
+}
+
 func (a *adapter) DirectoryExists(path tspath.Path) bool {
 	stat := a.stat(path)
 	return stat != nil && stat.IsDir()
@@ -153,7 +223,8 @@ func (a *adapter) GetDirectories(path tspath.Path) []string {
 		return nil
 	}
 
-	dirs := make([]string, 0, len(entries))
+	// TODO: should this really exist? ReadDir with manual filtering seems like a better idea.
+	var dirs []string
 	for _, entry := range entries {
 		if entry.IsDir() {
 			dirs = append(dirs, entry.Name())
@@ -162,14 +233,14 @@ func (a *adapter) GetDirectories(path tspath.Path) []string {
 	return dirs
 }
 
-func (a *adapter) ReadFile(path tspath.Path) string {
-	fsys, rest := a.rootAndPath(path)
+func (a *adapter) WalkDir(root tspath.Path, walkFn WalkDirFunc) error {
+	fsys, rest := a.rootAndPath(root)
 	if fsys == nil {
-		return ""
+		return nil
 	}
-	contents, err := fs.ReadFile(fsys, rest)
-	if err != nil {
-		return ""
-	}
-	return string(contents)
+
+	toPath := a.ToPath
+	return fs.WalkDir(fsys, rest, func(path string, d fs.DirEntry, err error) error {
+		return walkFn(toPath(path), d, err)
+	})
 }
