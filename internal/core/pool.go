d0fec03a6ec3dd00d4c0cf2973ed6a8bace60e58
Anders Hejlsberg <andersh@microsoft.com>
2024-11-16 14:08:48 -0800

    Add comments

--- internal/core/pool.go
+++ internal/core/pool.go
@@ -6,6 +6,8 @@ type Pool[T any] struct {
 	data []T
 }
 
+// Allocate a single element in the pool and return a pointer to the element. If the pool is at capacity,
+// a new pool of the next size up is allocated.
 func (p *Pool[T]) New() *T {
 	if len(p.data) == cap(p.data) {
 		p.data = make([]T, 0, NextPoolSize(len(p.data)))
@@ -17,7 +19,8 @@ func (p *Pool[T]) New() *T {
 
 // Allocate a slice of the given size in the pool. If the requested size is beyond the capacity of the pool
 // and a pool of the next size up still wouldn't fit the slice, make a separate memory allocation for the slice.
-// Otherwise, grow the pool if necessary and allocate a slice out of it.
+// Otherwise, grow the pool if necessary and allocate a slice out of it. The length and capacity of the resulting
+// slice are equal to the given size.
 func (p *Pool[T]) NewSlice(size int) []T {
 	if size == 0 {
 		return nil
