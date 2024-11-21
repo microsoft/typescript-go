e07a2762ed460921d577485628d4117eb7a1b5ce
Andrew Branch <andrew@wheream.io>
2024-11-21 15:22:38 -0800

    Implement ResolveTypeReferenceDirective

--- internal/compiler/module/util.go
+++ internal/compiler/module/util.go
@@ -3,8 +3,11 @@ package module
 import (
 	"strings"
 
+	"github.com/microsoft/typescript-go/internal/core"
 	"github.com/microsoft/typescript-go/internal/tspath"
 )
+
+var InferredTypesContainingFile = "__inferred type names__.ts"
 
 func ParseNodeModuleFromPath(resolved string, isFolder bool) string {
 	path := tspath.NormalizePath(resolved)
@@ -30,4 +33,47 @@ func ParsePackageName(moduleName string) (packageName, rest string) {
 		return moduleName, ""
 	}
 	return moduleName[:idx], moduleName[idx+1:]
+}
+
+func GetEffectiveTypeRoots(options *core.CompilerOptions, currentDirectory string) (result []string, fromConfig bool) {
+	if options.TypeRoots != nil {
+		return options.TypeRoots, true
+	}
+	var baseDir string
+	if options.ConfigFilePath != "" {
+		baseDir = tspath.GetDirectoryPath(options.ConfigFilePath)
+	} else {
+		baseDir = currentDirectory
+		if baseDir == "" {
+			// This was accounted for in the TS codebase, but only for third-party API usage
+			// where the module resolution host does not provide a getCurrentDirectory().
+			panic("cannot get effective type roots without a config file path or current directory")
+		}
+	}
+
+	typeRoots := make([]string, 0, strings.Count(baseDir, "/"))
+	tspath.ForEachAncestorDirectory(baseDir, func(dir string) (any, bool) {
+		typeRoots = append(typeRoots, tspath.CombinePaths(dir, "node_modules", "@types"))
+		return nil, false
+	})
+	return typeRoots, false
+}
+
+func MangleScopedPackageName(packageName string) string {
+	if packageName[0] == '@' {
+		idx := strings.Index(packageName, "/")
+		if idx == -1 {
+			return packageName
+		}
+		return packageName[1:idx] + "__" + packageName[idx+1:]
+	}
+	return packageName
+}
+
+func UnmangleScopedPackageName(packageName string) string {
+	idx := strings.Index(packageName, "__")
+	if idx != -1 {
+		return "@" + packageName[:idx] + "/" + packageName[idx+2:]
+	}
+	return packageName
 }
