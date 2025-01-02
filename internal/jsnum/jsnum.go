ac87bf510c14853394f3e38cd6a624700b0c6c69
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2025-01-02 15:00:28 -0800

    Add jsnum package, use

---
+++ internal/jsnum/jsnum.go
@@ -0,0 +1,72 @@
+// Package jsnum provides JS-like number handling.
+package jsnum
+
+import "math"
+
+// https://262.ecma-international.org/#sec-touint32
+func toUint32(x float64) uint32 {
+	// Fast path: if the number is the range (-2^31, 2^32), i.e. an SMI,
+	// then we don't need to do any special mapping.
+	if smi := int32(x); float64(smi) == x {
+		return uint32(smi)
+	}
+
+	// If the number is non-finite, it maps to zero.
+	if math.IsNaN(x) || math.IsInf(x, 0) {
+		return 0
+	}
+
+	// Otherwise, take x modulo 2^32, mapping positive numbers
+	// to [0, 2^32) and negative numbers to (-2^32, -0.0].
+	x = math.Mod(x, 1<<32)
+
+	// Convert to uint32, which will wrap negative numbers.
+	return uint32(x)
+}
+
+// https://262.ecma-international.org/#sec-toint32
+func toInt32(x float64) int32 {
+	// The only difference between ToUint32 and ToInt32 is the interpretation of the bits.
+	return int32(toUint32(x))
+}
+
+func toShiftCount(x float64) uint32 {
+	return toUint32(x) & 31
+}
+
+// https://262.ecma-international.org/#sec-numeric-types-number-signedRightShift
+func SignedRightShift(x, y float64) float64 {
+	return float64(toInt32(x) >> toShiftCount(y))
+}
+
+// https://262.ecma-international.org/#sec-numeric-types-number-unsignedRightShift
+func UnsignedRightShift(x, y float64) float64 {
+	return float64(toUint32(x) >> toShiftCount(y))
+}
+
+// https://262.ecma-international.org/#sec-numeric-types-number-leftShift
+func LeftShift(x, y float64) float64 {
+	return float64(toInt32(x) << toShiftCount(y))
+}
+
+// https://262.ecma-international.org/#sec-numeric-types-number-bitwiseNOT
+func BitwiseNOT(x float64) float64 {
+	return float64(^toInt32(x))
+}
+
+// The below are implemented by https://262.ecma-international.org/#sec-numberbitwiseop.
+
+// https://262.ecma-international.org/#sec-numeric-types-number-bitwiseOR
+func BitwiseOR(x, y float64) float64 {
+	return float64(toInt32(x) | toInt32(y))
+}
+
+// https://262.ecma-international.org/#sec-numeric-types-number-bitwiseAND
+func BitwiseAND(x, y float64) float64 {
+	return float64(toInt32(x) & toInt32(y))
+}
+
+// https://262.ecma-international.org/#sec-numeric-types-number-bitwiseXOR
+func BitwiseXOR(x, y float64) float64 {
+	return float64(toInt32(x) ^ toInt32(y))
+}
