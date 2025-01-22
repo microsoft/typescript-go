00114a25e56dd1fe7d2ce068c8c3f0a79e2127af
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2025-01-22 14:39:29 -0800

    Fix oops

--- internal/compiler/fileloader.go
+++ internal/compiler/fileloader.go
@@ -218,10 +218,12 @@ func (t *parseTask) start(loader *fileLoader) {
 func (p *fileLoader) parseSourceFile(fileName string) *ast.SourceFile {
 	path := tspath.ToPath(fileName, p.host.GetCurrentDirectory(), p.host.FS().UseCaseSensitiveFileNames())
 	text, _ := p.host.FS().ReadFile(fileName)
+	var sourceFile *ast.SourceFile
 	if tspath.FileExtensionIs(fileName, tspath.ExtensionJson) {
-		return parser.ParseJSONText(fileName, text)
+		sourceFile = parser.ParseJSONText(fileName, text)
+	} else {
+		sourceFile = parser.ParseSourceFile(fileName, text, p.compilerOptions.GetEmitScriptTarget())
 	}
-	sourceFile := parser.ParseSourceFile(fileName, text, p.compilerOptions.GetEmitScriptTarget())
 	sourceFile.SetPath(path)
 	return sourceFile
 }
