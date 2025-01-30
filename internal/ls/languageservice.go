a344b1d31c42b7833675adbf25942d35b93d6532
Andrew Branch <andrew@wheream.io>
2025-01-30 11:16:29 -0800

    Split up files

--- internal/ls/languageservice.go
+++ internal/ls/languageservice.go
@@ -7,19 +7,6 @@ import (
 	"github.com/microsoft/typescript-go/internal/vfs"
 )
 
-type Host interface {
-	FS() vfs.FS
-	GetCurrentDirectory() string
-	NewLine() string
-	Trace(msg string)
-	GetProjectVersion() int
-	// GetRootFileNames was called GetScriptFileNames in the original code.
-	GetRootFileNames() []string
-	// GetCompilerOptions was called GetCompilationSettings in the original code.
-	GetCompilerOptions() *core.CompilerOptions
-	GetSourceFile(fileName string, languageVersion core.ScriptTarget) *ast.SourceFile
-}
-
 var _ compiler.CompilerHost = (*LanguageService)(nil)
 
 type LanguageService struct {
