1e419bb8ee6474f2a30ef5e10ec7445cc7fc580d
Gabriela Araujo Britto <gabrielaa@microsoft.com>
2025-02-03 16:38:56 -0800

    oops. actually fix it

--- internal/testutil/harnessutil/harnessutil.go
+++ internal/testutil/harnessutil/harnessutil.go
@@ -516,11 +516,11 @@ func GetFileBasedTestConfigurations(t *testing.T, settings map[string]string, va
 					t.Fatal("Provided test options exceeded the maximum number of variations")
 				}
 				optionEntries = append(optionEntries, append([]string{option}, entries...))
+				continue
 			}
-		} else {
-			// Variation is not supported or not needed for the option
-			nonVariyingOptions[option] = value
 		}
+		// Variation is not supported or not needed for the option
+		nonVariyingOptions[option] = value
 	}
 
 	var configurations []*NamedTestConfiguration
