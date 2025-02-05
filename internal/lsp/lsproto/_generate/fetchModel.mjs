bbbab17500c0a7b65468f4606b46eeecbd469b41
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2025-02-05 11:50:29 -0800

    Switch to antfu's package

--- internal/lsp/lsproto/_generate/fetchModel.mjs
+++ internal/lsp/lsproto/_generate/fetchModel.mjs
@@ -1,4 +1,4 @@
-import { compile } from "json-schema-to-typescript";
+import { compile } from "json-schema-to-typescript-lite";
 import fs from "node:fs";
 import path from "node:path";
 import url from "node:url";
