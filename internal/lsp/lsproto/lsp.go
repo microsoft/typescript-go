063e795dad995cb74f6491ae646d2544aa8d4726
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2025-02-04 19:03:10 -0800

    move

---
+++ internal/lsp/lsproto/lsp.go
@@ -0,0 +1,4 @@
+package lsproto
+
+//go:generate node --experimental-strip-types ./_generate/generate.mts lsp_generated.go
+//go:generate gofumpt -w lsp_generated.go
--- internal/lsp/lsproto/lsproto2.go
+++
@@ -1,4 +0,0 @@
-package lsproto
-
-//go:generate node --experimental-strip-types ../../../_tools/genlsp/generate.mts lsp_generated.go
-//go:generate gofumpt -w lsp_generated.go
