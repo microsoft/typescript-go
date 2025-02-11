f783ecd51dcb58e5af5418cc7a33e56d2c265252
Isabel Duan <isabelduan@microsoft.com>
2025-02-11 12:35:24 -0800

    Revert "comments"
    This reverts commit ada7863d8fad42db91bb81782a332d711894d31a.

--- cmd/tsgo/watch.go
+++ cmd/tsgo/watch.go
@@ -10,7 +10,6 @@ import (
 func start(watcher execute.Watcher) int {
 	watchInterval := 100 * time.Millisecond
 	for {
-		// todo: for non interval based watch, wait for file change event here
 		fmt.Fprint(watcher.Sys().Writer(), "build starting at ", time.Now(), "\n")
 		watcher.CompileAndEmit()
 		fmt.Fprint(watcher.Sys().Writer(), "build finished ", time.Now(), "\n")
