9a3b69ada1668215f09850208ef2a419fad26ba2
Ron Buckton <ron.buckton@microsoft.com>
2025-02-12 12:52:26 -0500

    Align module/enum downlevel parenthesization with old emitter

--- internal/transformers/runtimesyntax_test.go
+++ internal/transformers/runtimesyntax_test.go
@@ -19,31 +19,31 @@ func TestEnumTransformer(t *testing.T) {
 	}{
 		{title: "empty enum", input: "enum E {}", output: `var E;
 (function (E) {
-}(E || (E = {})));`},
+})(E || (E = {}));`},
 
 		{title: "simple enum", input: "enum E {A}", output: `var E;
 (function (E) {
     E[E["A"] = 0] = "A";
-}(E || (E = {})));`},
+})(E || (E = {}));`},
 
 		{title: "autonumber enum #1", input: "enum E {A,B}", output: `var E;
 (function (E) {
     E[E["A"] = 0] = "A";
     E[E["B"] = 1] = "B";
-}(E || (E = {})));`},
+})(E || (E = {}));`},
 
 		{title: "autonumber enum #2", input: "enum E {A = 1,B}", output: `var E;
 (function (E) {
     E[E["A"] = 1] = "A";
     E[E["B"] = 2] = "B";
-}(E || (E = {})));`},
+})(E || (E = {}));`},
 
 		{title: "autonumber enum #3", input: "enum E {A = 1,B,C}", output: `var E;
 (function (E) {
     E[E["A"] = 1] = "A";
     E[E["B"] = 2] = "B";
     E[E["C"] = 3] = "C";
-}(E || (E = {})));`},
+})(E || (E = {}));`},
 
 		{title: "autonumber enum #4", input: "enum E {A = x,B,C}", output: `var E;
 (function (E) {
@@ -51,7 +51,7 @@ func TestEnumTransformer(t *testing.T) {
     E[E["A"] = auto = x] = "A";
     E[E["B"] = ++auto] = "B";
     E[E["C"] = ++auto] = "C";
-}(E || (E = {})));`},
+})(E || (E = {}));`},
 
 		{title: "autonumber enum #5", input: "enum E {A = x,B,C = y}", output: `var E;
 (function (E) {
@@ -60,7 +60,7 @@ func TestEnumTransformer(t *testing.T) {
     E[E["B"] = ++auto] = "B";
     E["C"] = y;
     if (typeof E.C !== "string") E[E.C] = "C";
-}(E || (E = {})));`},
+})(E || (E = {}));`},
 
 		{title: "autonumber enum #6", input: "enum E {A = x,B = y,C = z}", output: `var E;
 (function (E) {
@@ -70,7 +70,7 @@ func TestEnumTransformer(t *testing.T) {
     if (typeof E.B !== "string") E[E.B] = "B";
     E["C"] = z;
     if (typeof E.C !== "string") E[E.C] = "C";
-}(E || (E = {})));`},
+})(E || (E = {}));`},
 
 		{title: "autonumber enum #7", input: "enum E {A = 1,B,C,D='x'}", output: `var E;
 (function (E) {
@@ -78,21 +78,21 @@ func TestEnumTransformer(t *testing.T) {
     E[E["B"] = 2] = "B";
     E[E["C"] = 3] = "C";
     E["D"] = 'x';
-}(E || (E = {})));`},
+})(E || (E = {}));`},
 
 		{title: "autonumber enum #8", input: "enum E {A,B=2,C}", output: `var E;
 (function (E) {
     E[E["A"] = 0] = "A";
     E[E["B"] = 2] = "B";
     E[E["C"] = 3] = "C";
-}(E || (E = {})));`},
+})(E || (E = {}));`},
 
 		{title: "autonumber enum #9", input: "enum E {A='x',B=2,C}", output: `var E;
 (function (E) {
     E["A"] = 'x';
     E[E["B"] = 2] = "B";
     E[E["C"] = 3] = "C";
-}(E || (E = {})));`},
+})(E || (E = {}));`},
 
 		{title: "autonumber enum #10", input: "enum E {A='x',B=y,C}", output: `var E;
 (function (E) {
@@ -100,7 +100,7 @@ func TestEnumTransformer(t *testing.T) {
     E["A"] = 'x';
     E[E["B"] = auto = y] = "B";
     E[E["C"] = ++auto] = "C";
-}(E || (E = {})));`},
+})(E || (E = {}));`},
 
 		{title: "autonumber enum #11", input: "enum E {A='x',B=1,C,D=y,E,F=3,G}", output: `var E;
 (function (E) {
@@ -112,19 +112,19 @@ func TestEnumTransformer(t *testing.T) {
     E[E["E"] = ++auto] = "E";
     E[E["F"] = 3] = "F";
     E[E["G"] = 4] = "G";
-}(E || (E = {})));`},
+})(E || (E = {}));`},
 
 		{title: "autonumber enum #12", input: "enum E {A=-1,B}", output: `var E;
 (function (E) {
     E[E["A"] = -1] = "A";
     E[E["B"] = 0] = "B";
-}(E || (E = {})));`},
+})(E || (E = {}));`},
 
 		{title: "autonumber enum #13", input: "enum E {A='x',B}", output: `var E;
 (function (E) {
     E["A"] = 'x';
     E["B"] = void 0;
-}(E || (E = {})));`},
+})(E || (E = {}));`},
 
 		{title: "autonumber enum #14", input: "enum E {A,B,C=A|B,D}", output: `var E;
 (function (E) {
@@ -133,28 +133,28 @@ func TestEnumTransformer(t *testing.T) {
     E[E["B"] = 1] = "B";
     E[E["C"] = auto = E.A | E.B] = "C";
     E[E["D"] = ++auto] = "D";
-}(E || (E = {})));`},
+})(E || (E = {}));`},
 
 		{title: "string enum", input: "enum E {A = 'x',B = 'y',C = 'z'}", output: `var E;
 (function (E) {
     E["A"] = 'x';
     E["B"] = 'y';
     E["C"] = 'z';
-}(E || (E = {})));`},
+})(E || (E = {}));`},
 
 		{title: "number enum", input: "enum E {A = 0,B = 1,C = 2}", output: `var E;
 (function (E) {
     E[E["A"] = 0] = "A";
     E[E["B"] = 1] = "B";
     E[E["C"] = 2] = "C";
-}(E || (E = {})));`},
+})(E || (E = {}));`},
 
 		{title: "enum self reference #1", input: "enum E {A,B=A}", output: `var E;
 (function (E) {
     E[E["A"] = 0] = "A";
     E["B"] = E.A;
     if (typeof E.B !== "string") E[E.B] = "B";
-}(E || (E = {})));`},
+})(E || (E = {}));`},
 
 		{title: "enum self reference #2", input: "enum E {A=x,B=A}", output: `var E;
 (function (E) {
@@ -162,7 +162,7 @@ func TestEnumTransformer(t *testing.T) {
     if (typeof E.A !== "string") E[E.A] = "A";
     E["B"] = E.A;
     if (typeof E.B !== "string") E[E.B] = "B";
-}(E || (E = {})));`},
+})(E || (E = {}));`},
 
 		{title: "enum self reference #3", input: "enum E {'A'=x,B=A}", output: `var E;
 (function (E) {
@@ -170,7 +170,7 @@ func TestEnumTransformer(t *testing.T) {
     if (typeof E["A"] !== "string") E[E["A"]] = "A";
     E["B"] = E.A;
     if (typeof E.B !== "string") E[E.B] = "B";
-}(E || (E = {})));`},
+})(E || (E = {}));`},
 
 		{title: "enum self reference #4", input: "enum E {'A'=x,'B '=A}", output: `var E;
 (function (E) {
@@ -178,28 +178,28 @@ func TestEnumTransformer(t *testing.T) {
     if (typeof E["A"] !== "string") E[E["A"]] = "A";
     E["B "] = E.A;
     if (typeof E["B "] !== "string") E[E["B "]] = "B ";
-}(E || (E = {})));`},
+})(E || (E = {}));`},
 
 		{title: "export enum", input: "export enum E {A, B}", output: `export var E;
 (function (E) {
     E[E["A"] = 0] = "A";
     E[E["B"] = 1] = "B";
-}(E || (E = {})));`},
+})(E || (E = {}));`},
 
 		{title: "const enum", input: "const enum E {A, B}", output: `var E;
 (function (E) {
     E[E["A"] = 0] = "A";
     E[E["B"] = 1] = "B";
-}(E || (E = {})));`},
+})(E || (E = {}));`},
 
 		{title: "merged enum", input: "enum E {A} enum E {B=A}", output: `var E;
 (function (E) {
     E[E["A"] = 0] = "A";
-}(E || (E = {})));
+})(E || (E = {}));
 (function (E) {
     E["B"] = A;
     if (typeof E.B !== "string") E[E.B] = "B";
-}(E || (E = {})));`},
+})(E || (E = {}));`},
 	}
 
 	for _, rec := range data {
@@ -224,123 +224,123 @@ func TestNamespaceTransformer(t *testing.T) {
 	}{
 		{title: "empty namespace", input: "namespace N {}", output: `var N;
 (function (N) {
-}(N || (N = {})));`},
+})(N || (N = {}));`},
 
 		{title: "export var", input: "namespace N { export var x = 1; }", output: `var N;
 (function (N) {
     N.x = 1;
-}(N || (N = {})));`},
+})(N || (N = {}));`},
 
 		{title: "export uninitialized var", input: "namespace N { export var x; }", output: `var N;
 (function (N) {
-}(N || (N = {})));`},
+})(N || (N = {}));`},
 
 		{title: "exported var reference", input: "namespace N { export var x = 1; x; }", output: `var N;
 (function (N) {
     N.x = 1;
     N.x;
-}(N || (N = {})));`},
+})(N || (N = {}));`},
 
 		{title: "exported var reference across namespaces", input: "namespace N { export var x = 1; } namespace N { x; }", output: `var N;
 (function (N) {
     N.x = 1;
-}(N || (N = {})));
+})(N || (N = {}));
 (function (N) {
     x;
-}(N || (N = {})));`},
+})(N || (N = {}));`},
 
 		{title: "exported array binding pattern", input: "namespace N { export var [x] = [1]; }", output: `var N;
 (function (N) {
     [N.x] = [1];
-}(N || (N = {})));`},
+})(N || (N = {}));`},
 
 		{title: "exported array binding pattern + initializer", input: "namespace N { export var [x = 2] = [1]; }", output: `var N;
 (function (N) {
     [N.x = 2] = [1];
-}(N || (N = {})));`},
+})(N || (N = {}));`},
 
 		{title: "exported array binding pattern + elision", input: "namespace N { export var [, x] = [1]; }", output: `var N;
 (function (N) {
     [, N.x] = [1];
-}(N || (N = {})));`},
+})(N || (N = {}));`},
 
 		{title: "exported array binding pattern + rest", input: "namespace N { export var [, ...x] = [1]; }", output: `var N;
 (function (N) {
     [, ...N.x] = [1];
-}(N || (N = {})));`},
+})(N || (N = {}));`},
 
 		{title: "exported array binding pattern + nested array pattern", input: "namespace N { export var [[x]] = [[1]]; }", output: `var N;
 (function (N) {
     [[N.x]] = [[1]];
-}(N || (N = {})));`},
+})(N || (N = {}));`},
 
 		{title: "exported array binding pattern + nested object pattern", input: "namespace N { export var [{x}] = [{x: 1}]; }", output: `var N;
 (function (N) {
     [{ x: N.x }] = [{ x: 1 }];
-}(N || (N = {})));`},
+})(N || (N = {}));`},
 
 		{title: "exported object binding pattern", input: "namespace N { export var {x: x} = {x: 1}; }", output: `var N;
 (function (N) {
     ({ x: N.x } = { x: 1 });
-}(N || (N = {})));`},
+})(N || (N = {}));`},
 
 		{title: "exported object binding pattern + shorthand assignment", input: "namespace N { export var {x} = {x: 1}; }", output: `var N;
 (function (N) {
     ({ x: N.x } = { x: 1 });
-}(N || (N = {})));`},
+})(N || (N = {}));`},
 
 		{title: "exported object binding pattern + initializer", input: "namespace N { export var {x: x = 2} = {x: 1}; }", output: `var N;
 (function (N) {
     ({ x: N.x = 2 } = { x: 1 });
-}(N || (N = {})));`},
+})(N || (N = {}));`},
 
 		{title: "exported object binding pattern + shorthand assignment + initializer", input: "namespace N { export var {x = 2} = {x: 1}; }", output: `var N;
 (function (N) {
     ({ x: N.x = 2 } = { x: 1 });
-}(N || (N = {})));`},
+})(N || (N = {}));`},
 
 		{title: "exported object binding pattern + rest", input: "namespace N { export var {...x} = {x: 1}; }", output: `var N;
 (function (N) {
     ({ ...N.x } = { x: 1 });
-}(N || (N = {})));`},
+})(N || (N = {}));`},
 
 		{title: "exported object binding pattern + nested object pattern", input: "namespace N { export var {y:{x}} = {y: {x: 1}}; }", output: `var N;
 (function (N) {
     ({ y: { x: N.x } } = { y: { x: 1 } });
-}(N || (N = {})));`},
+})(N || (N = {}));`},
 
 		{title: "exported object binding pattern + nested array pattern", input: "namespace N { export var {y:[x]} = {y: [1]}; }", output: `var N;
 (function (N) {
     ({ y: [N.x] } = { y: [1] });
-}(N || (N = {})));`},
+})(N || (N = {}));`},
 
 		{title: "export function", input: "namespace N { export function f() {} }", output: `var N;
 (function (N) {
     function f() { }
     N.f = f;
-}(N || (N = {})));`},
+})(N || (N = {}));`},
 
 		{title: "export class", input: "namespace N { export class C {} }", output: `var N;
 (function (N) {
     class C {
     }
     N.C = C;
-}(N || (N = {})));`},
+})(N || (N = {}));`},
 
 		{title: "export enum", input: "namespace N { export enum E {A} }", output: `var N;
 (function (N) {
     let E;
     (function (E) {
         E[E["A"] = 0] = "A";
-    }(E = N.E || (N.E = {})));
-}(N || (N = {})));`},
+    })(E = N.E || (N.E = {}));
+})(N || (N = {}));`},
 
 		{title: "export namespace", input: "namespace N { export namespace N2 {} }", output: `var N;
 (function (N) {
     let N2;
     (function (N2) {
-    }(N2 = N.N2 || (N.N2 = {})));
-}(N || (N = {})));`},
+    })(N2 = N.N2 || (N.N2 = {}));
+})(N || (N = {}));`},
 
 		{title: "import=", input: "import X = Y.X;", output: `var X = Y.X;`},
 
@@ -349,18 +349,18 @@ func TestNamespaceTransformer(t *testing.T) {
 		{title: "export import= in namespace", input: "namespace N { export import X = Y.X; }", output: `var N;
 (function (N) {
     N.X = Y.X;
-}(N || (N = {})));`},
+})(N || (N = {}));`},
 
 		{title: "shorthand property assignment", input: "namespace N { export var x = 1; var y = { x }; }", output: `var N;
 (function (N) {
     N.x = 1;
     var y = { x: N.x };
-}(N || (N = {})));`},
+})(N || (N = {}));`},
 
 		{title: "shorthand property assignment pattern", input: "namespace N { export var x; ({x} = {x: 1}); }", output: `var N;
 (function (N) {
     ({ x: N.x } = { x: 1 });
-}(N || (N = {})));`},
+})(N || (N = {}));`},
 	}
 
 	for _, rec := range data {
