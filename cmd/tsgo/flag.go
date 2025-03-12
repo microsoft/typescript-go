2d60b6dd93e053787e0de4c05b4cf38f98a1af92
zjkung1123 <zjkung1123@gmail.com>
2025-03-11 22:41:06 -0400

    Refactor tristateFlag string literals for consistency and clarity

--- cmd/tsgo/flag.go
+++ cmd/tsgo/flag.go
@@ -11,6 +11,12 @@ var errParse = errors.New("parse error")
 
 type tristateFlag core.Tristate
 
+const (
+ True string = "true"
+ False       = "false"
+ Unset       = "unset"
+)
+
 func (f *tristateFlag) Set(s string) error {
 	v, err := strconv.ParseBool(s)
 	if err != nil {
@@ -27,11 +33,11 @@ func (f *tristateFlag) Set(s string) error {
 func (f *tristateFlag) String() string {
 	switch core.Tristate(*f) {
 	case core.TSTrue:
-		return "true"
+		return True
 	case core.TSFalse:
-		return "false"
+		return False
 	default:
-		return "unset"
+		return Unset
 	}
 }
 
