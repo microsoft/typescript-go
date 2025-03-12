ed8dcde3a88907dfb9f72a43a699ac630a33c2e7
zjkung1123 <zjkung1123@gmail.com>
2025-03-11 22:35:18 -0400

    Refactor tristateFlag methods for improved readability and consistency, fix typo

--- cmd/tsgo/flag.go
+++ cmd/tsgo/flag.go
@@ -1,44 +1,50 @@
 package main
 
 import (
-	"errors"
-	"strconv"
+    "errors"
+    "strconv"
 
-	"github.com/microsoft/typescript-go/internal/core"
+    "github.com/microsoft/typescript-go/internal/core"
 )
 
 var errParse = errors.New("parse error")
 
 type tristateFlag core.Tristate
 
+const (
+    True string  = "true"
+    False        = "false"
+    Unset        = "unset"
+)
+
 func (f *tristateFlag) Set(s string) error {
-	v, err := strconv.ParseBool(s)
-	if err != nil {
-		return errParse
-	}
-	if v {
-		*f = (tristateFlag)(core.TSTrue)
-	} else {
-		*f = (tristateFlag)(core.TSFalse)
-	}
-	return nil
+    ok, err := strconv.ParseBool(s)
+    if err != nil {
+        return errParse
+    }
+    if ok {
+        *f = (tristateFlag)(core.TSTrue)
+    } else {
+        *f = (tristateFlag)(core.TSFalse)
+    }
+    return nil
 }
 
 func (f *tristateFlag) String() string {
-	switch core.Tristate(*f) {
-	case core.TSTrue:
-		return "true"
-	case core.TSFalse:
-		return "false"
-	default:
-		return "unset"
-	}
+    switch core.Tristate(*f) {
+    case core.TSTrue:
+        return True
+    case core.TSFalse:
+        return False
+    default:
+        return Unset
+    }
 }
 
 func (f *tristateFlag) Get() any {
-	return core.Tristate(*f)
+    return core.Tristate(*f)
 }
 
 func (f *tristateFlag) IsBoolFlag() bool {
-	return true
+    return true
 }
