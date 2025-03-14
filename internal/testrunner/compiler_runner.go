7a6410541abe8ce82d52dab2b53100145ff00866
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2025-03-13 19:55:37 -0700

    Duplicate test checking, small refactor

--- internal/testrunner/compiler_runner.go
+++ internal/testrunner/compiler_runner.go
@@ -56,8 +56,6 @@ type CompilerBaselineRunner struct {
 	testSuitName string
 }
 
-var _ Runner = (*CompilerBaselineRunner)(nil)
-
 func NewCompilerBaselineRunner(testType CompilerTestType, isSubmodule bool) *CompilerBaselineRunner {
 	testSuitName := testType.String()
 	var basePath string
--- internal/testrunner/compiler_runner_test.go
+++ internal/testrunner/compiler_runner_test.go
@@ -4,31 +4,19 @@ import (
 	"testing"
 
 	"github.com/microsoft/typescript-go/internal/bundled"
-	"github.com/microsoft/typescript-go/internal/repo"
+	"github.com/microsoft/typescript-go/internal/core"
+	"gotest.tools/v3/assert"
 )
 
 // Runs the new compiler tests and produces baselines (e.g. `test1.symbols`).
-func TestLocal(t *testing.T) {
-	t.Parallel()
-
-	if !bundled.Embedded {
-		// Without embedding, we'd need to read all of the lib files out from disk into the MapFS.
-		// Just skip this for now.
-		t.Skip("bundled files are not embedded")
-	}
-
-	testTypes := []CompilerTestType{TestTypeRegression, TestTypeConformance}
-	for _, testType := range testTypes {
-		runner := NewCompilerBaselineRunner(testType, false /*isSubmodule*/)
-		runner.RunTests(t)
-	}
-}
+func TestLocal(t *testing.T) { runTests(t, false) } //nolint:paralleltest
 
 // Runs the old compiler tests, and produces new baselines (e.g. `test1.symbols`)
 // and a diff between the new and old baselines (e.g. `test1.symbols.diff`).
-func TestSubmodule(t *testing.T) {
+func TestSubmodule(t *testing.T) { runTests(t, true) } //nolint:paralleltest
+
+func runTests(t *testing.T, isSubmodule bool) {
 	t.Parallel()
-	repo.SkipIfNoTypeScriptSubmodule(t)
 
 	if !bundled.Embedded {
 		// Without embedding, we'd need to read all of the lib files out from disk into the MapFS.
@@ -36,9 +24,20 @@ func TestSubmodule(t *testing.T) {
 		t.Skip("bundled files are not embedded")
 	}
 
-	testTypes := []CompilerTestType{TestTypeRegression, TestTypeConformance}
-	for _, testType := range testTypes {
-		runner := NewCompilerBaselineRunner(testType, true /*isSubmodule*/)
+	runners := []*CompilerBaselineRunner{
+		NewCompilerBaselineRunner(TestTypeRegression, isSubmodule),
+		NewCompilerBaselineRunner(TestTypeConformance, isSubmodule),
+	}
+
+	var seenTests core.Set[string]
+	for _, runner := range runners {
+		for _, test := range runner.EnumerateTestFiles() {
+			assert.Assert(t, !seenTests.Has(test), "Duplicate test file: %s", test)
+			seenTests.Add(test)
+		}
+	}
+
+	for _, runner := range runners {
 		runner.RunTests(t)
 	}
 }
