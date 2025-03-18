d86780ddb0e4e00f63b18fc7b54250619adbef17
Fróði Karlsson <frodikarlsson@gmail.com>
2025-03-19 00:30:42 +0100

    Remove unnecessary struct

--- internal/printer/sourcefilemetadataprovider.go
+++ internal/printer/sourcefilemetadataprovider.go
@@ -8,11 +8,3 @@ import (
 type SourceFileMetaDataProvider interface {
 	GetSourceFileMetaData(path tspath.Path) *ast.SourceFileMetaData
 }
-
-type sourceFileMetadataProvider struct {
-	getSourceFileMetaData func(path tspath.Path) *ast.SourceFileMetaData
-}
-
-func (r *sourceFileMetadataProvider) GetSourceFileMetaData(path tspath.Path) *ast.SourceFileMetaData {
-	return r.getSourceFileMetaData(path)
-}
