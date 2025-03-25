90495ff9eb9a50548868046fb163145275556337
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2025-03-25 10:44:49 -0700

    Add test

---
+++ testdata/baselines/reference/config/tsconfigParsing/handles empty types array with jsonSourceFile api.js
@@ -0,0 +1,22 @@
+Fs::
+//// [/app.ts]
+
+
+//// [/tsconfig.json]
+{
+			    "compilerOptions": {
+					"types": []
+				}
+			}
+
+
+configFileName:: tsconfig.json
+CompilerOptions::
+{
+  "configFilePath": "/tsconfig.json"
+}
+
+FileNames::
+/app.ts
+Errors::
+
