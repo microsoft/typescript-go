4f9e657fccac0613dcf4b68032974f4c008eec83
Mateusz Burzyński <mateuszburzynski@gmail.com>
2025-03-27 10:33:56 +0100

    dont use expanded parameters when they have a non-trailing rest

--- internal/checker/printer.go
+++ internal/checker/printer.go
@@ -478,7 +478,12 @@ func (p *Printer) printSignature(sig *Signature, returnSeparator string) {
 		p.printType(p.c.getTypeOfSymbol(sig.thisParameter))
 		tail = true
 	}
-	for i, param := range p.c.GetExpandedParameters(sig) {
+	expandedParameters := p.c.GetExpandedParameters(sig)
+	// If the expanded parameter list had a variadic in a non-trailing position, don't expand it
+	parameters := core.IfElse(core.Some(expandedParameters, func(s *ast.Symbol) bool {
+		return s != expandedParameters[len(expandedParameters)-1] && s.CheckFlags&ast.CheckFlagsRestParameter != 0
+	}), sig.parameters, expandedParameters)
+	for i, param := range parameters {
 		if tail {
 			p.print(", ")
 		}
