45a9003c8a32ea4aa989be1d7e3ae44fa1eb6a1e
Anders Hejlsberg <andersh@microsoft.com>
2025-04-22 11:55:29 -0400

    Merge related information in diagnostics

--- internal/ast/diagnostic.go
+++ internal/ast/diagnostic.go
@@ -60,6 +60,18 @@ func (d *Diagnostic) AddRelatedInfo(relatedInformation *Diagnostic) *Diagnostic 
 	return d
 }
 
+func (d *Diagnostic) Clone() *Diagnostic {
+	return &Diagnostic{
+		file:               d.file,
+		loc:                d.loc,
+		code:               d.code,
+		category:           d.category,
+		message:            d.message,
+		messageChain:       d.messageChain,
+		relatedInformation: d.relatedInformation,
+	}
+}
+
 func NewDiagnostic(file *SourceFile, loc core.TextRange, message *diagnostics.Message, args ...any) *Diagnostic {
 	return &Diagnostic{
 		file:     file,
@@ -137,12 +149,16 @@ func getDiagnosticPath(d *Diagnostic) string {
 }
 
 func EqualDiagnostics(d1, d2 *Diagnostic) bool {
+	return EqualDiagnosticsNoRelatedInfo(d1, d2) &&
+		slices.EqualFunc(d1.RelatedInformation(), d2.RelatedInformation(), EqualDiagnostics)
+}
+
+func EqualDiagnosticsNoRelatedInfo(d1, d2 *Diagnostic) bool {
 	return getDiagnosticPath(d1) == getDiagnosticPath(d2) &&
 		d1.Loc() == d2.Loc() &&
 		d1.Code() == d2.Code() &&
 		d1.Message() == d2.Message() &&
-		slices.EqualFunc(d1.MessageChain(), d2.MessageChain(), equalMessageChain) &&
-		slices.EqualFunc(d1.RelatedInformation(), d2.RelatedInformation(), EqualDiagnostics)
+		slices.EqualFunc(d1.MessageChain(), d2.MessageChain(), equalMessageChain)
 }
 
 func equalMessageChain(c1, c2 *Diagnostic) bool {
