b8bc14704a1212f8baf713ade6a4d8d890b3e0fa
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2025-04-28 09:51:11 -0700

    remove TODO that is now done

--- internal/parser/reparser.go
+++ internal/parser/reparser.go
@@ -79,7 +79,6 @@ func (p *Parser) reparseTags(parent *ast.Node, jsDoc []*ast.Node) {
 		for _, tag := range j.AsJSDoc().Tags.Nodes {
 			switch tag.Kind {
 			case ast.KindJSDocTypedefTag:
-				// !!! Don't mark typedefs as exported if they are not in a module
 				typeExpression := tag.AsJSDocTypedefTag().TypeExpression
 				if typeExpression == nil {
 					break
