f4fe326473ecc5439b041b86984c16921a052556
Sean R. Abraham <seanabraham@gmail.com>
2025-05-08 11:33:18 -0400

    Improve the missing CompilerOptions check
    This mucks with the real code a bit, but it works better and is simpler.
    
    See
    https://github.com/microsoft/typescript-go/pull/839
    https://github.com/microsoft/typescript-go/issues/842

--- internal/tsoptions/parsinghelpers_test.go
+++ internal/tsoptions/parsinghelpers_test.go
@@ -1,6 +1,7 @@
 package tsoptions
 
 import (
+	"fmt"
 	"reflect"
 	"strings"
 	"testing"
@@ -8,34 +9,36 @@ import (
 	"github.com/microsoft/typescript-go/internal/core"
 )
 
-func TestParseCompilerOptionNoMissingTristates(t *testing.T) {
+func TestParseCompilerOptionNoMissingFields(t *testing.T) {
 	t.Parallel()
-
 	var missingKeys []string
 	for _, field := range reflect.VisibleFields(reflect.TypeFor[core.CompilerOptions]()) {
-		keyName := field.Name
-		// use the JSON key from the tag, if present
-		// e.g. `json:"dog[,anythingelse]"` --> dog
-		if jsonTag, ok := field.Tag.Lookup("json"); ok {
-			keyName = strings.SplitN(jsonTag, ",", 2)[0]
-		}
-
-		isTristate := field.Type == reflect.TypeFor[core.Tristate]()
-		if isTristate {
-			// Set the field on a CompilerOptions to something other than the
-			// default (i.e. not TSUnknown), then check whether
-			// ParseCompilerOptions does actually update the value for that key.
-			testValue := core.TSTrue
+		func() {
+			defer func() {
+				if r := recover(); r != nil {
+					if strings.Contains(fmt.Sprint(r), "interface conversion") {
+						// this is a success case, amazingly
+						return
+					}
+					t.Errorf("unexpected panic: %v", r)
+				}
+			}()
+			keyName := field.Name
+			// use the JSON key from the tag, if present
+			// e.g. `json:"dog[,anythingelse]"` --> dog
+			if jsonTag, ok := field.Tag.Lookup("json"); ok {
+				keyName = strings.SplitN(jsonTag, ",", 2)[0]
+			}
+			var something any
 			co := core.CompilerOptions{}
-			ParseCompilerOptions(keyName, testValue, &co)
-			newSetValue := reflect.ValueOf(co).FieldByName(field.Name)
-			if !newSetValue.Equal(reflect.ValueOf(testValue)) {
+			found := parseCompilerOptions(keyName, something, &co)
+			if !found {
 				missingKeys = append(missingKeys, keyName)
 			}
-		}
+		}()
 	}
 	if len(missingKeys) > 0 {
-		t.Errorf("The following Tristate keys are missing entries in the ParseCompilerOptions"+
+		t.Errorf("The following keys are missing entries in the ParseCompilerOptions"+
 			" switch statement:\n%v", missingKeys)
 	}
 }
