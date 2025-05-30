448829a67bdd08efbbfb8d9a7e7e3f12b9c7e8fe
Sheetal Nandi <shkamat@microsoft.com>
2025-05-30 15:26:19 -0700

    Program to take ParsedCommandLine as config instead of separate options, root names, diagnostics etc

--- internal/compiler/program_test.go
+++ internal/compiler/program_test.go
@@ -232,9 +232,13 @@ func TestProgram(t *testing.T) {
 			opts := core.CompilerOptions{Target: testCase.target}
 
 			program := NewProgram(ProgramOptions{
-				RootFiles: []string{"c:/dev/src/index.ts"},
-				Host:      NewCompilerHost(&opts, "c:/dev/src", fs, bundled.LibPath()),
-				Options:   &opts,
+				Config: &tsoptions.ParsedCommandLine{
+					ParsedConfig: &core.ParsedOptions{
+						FileNames:       []string{"c:/dev/src/index.ts"},
+						CompilerOptions: &opts,
+					},
+				},
+				Host: NewCompilerHost(&opts, "c:/dev/src", fs, bundled.LibPath()),
 			})
 
 			actualFiles := []string{}
@@ -265,9 +269,13 @@ func BenchmarkNewProgram(b *testing.B) {
 
 			opts := core.CompilerOptions{Target: testCase.target}
 			programOpts := ProgramOptions{
-				RootFiles: []string{"c:/dev/src/index.ts"},
-				Host:      NewCompilerHost(&opts, "c:/dev/src", fs, bundled.LibPath()),
-				Options:   &opts,
+				Config: &tsoptions.ParsedCommandLine{
+					ParsedConfig: &core.ParsedOptions{
+						FileNames:       []string{"c:/dev/src/index.ts"},
+						CompilerOptions: &opts,
+					},
+				},
+				Host: NewCompilerHost(&opts, "c:/dev/src", fs, bundled.LibPath()),
 			}
 
 			for b.Loop() {
@@ -290,8 +298,8 @@ func BenchmarkNewProgram(b *testing.B) {
 		assert.Equal(b, len(errors), 0, "Expected no errors in parsed command line")
 
 		opts := ProgramOptions{
-			Host:    host,
-			Options: parsed.CompilerOptions(),
+			Config: parsed,
+			Host:   host,
 		}
 
 		for b.Loop() {
