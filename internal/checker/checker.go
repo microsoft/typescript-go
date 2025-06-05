a5a4449cb829928fe7c8d9e34233c92040921214
Mateusz Burzyński <mateuszburzynski@gmail.com>
2025-06-05 08:54:42 +0200

    include `nodeId` in the cache key insead

--- internal/checker/checker.go
+++ internal/checker/checker.go
@@ -226,6 +226,7 @@ type ReverseMappedTypeKey struct {
 type IterationTypesKey struct {
 	typeId TypeId
 	use    IterationUse
+	nodeId ast.NodeId
 }
 
 // FlowLoopKey
@@ -5881,26 +5882,22 @@ func (c *Checker) getIterationTypesOfIterable(t *Type, use IterationUse, errorNo
 	if IsTypeAny(t) {
 		return IterationTypes{c.anyType, c.anyType, c.anyType}
 	}
-	key := IterationTypesKey{typeId: t.id, use: use & IterationUseCacheFlags}
-	// If we are reporting errors and encounter a cached `noIterationTypes`, we should ignore the cached value and continue as if nothing was cached.
-	// In addition, we should not cache any new results for this call.
-	noCache := false
+	var nodeId ast.NodeId
+	if errorNode != nil {
+		nodeId = ast.GetNodeId(errorNode)
+	}
+	key := IterationTypesKey{typeId: t.id, use: use & IterationUseCacheFlags, nodeId: nodeId}
 	if cached, ok := c.iterationTypesCache[key]; ok {
-		if errorNode == nil || cached.hasTypes() {
-			return cached
-		}
-		noCache = true
+		return cached
 	}
-	result := c.getIterationTypesOfIterableWorker(t, use, errorNode, noCache)
-	if !noCache {
-		c.iterationTypesCache[key] = result
-	}
+	result := c.getIterationTypesOfIterableWorker(t, use, errorNode)
+	c.iterationTypesCache[key] = result
 	return result
 }
 
-func (c *Checker) getIterationTypesOfIterableWorker(t *Type, use IterationUse, errorNode *ast.Node, noCache bool) IterationTypes {
+func (c *Checker) getIterationTypesOfIterableWorker(t *Type, use IterationUse, errorNode *ast.Node) IterationTypes {
 	if t.flags&TypeFlagsUnion != 0 {
-		return c.combineIterationTypes(core.Map(t.Types(), func(t *Type) IterationTypes { return c.getIterationTypesOfIterableWorker(t, use, errorNode, noCache) }))
+		return c.combineIterationTypes(core.Map(t.Types(), func(t *Type) IterationTypes { return c.getIterationTypesOfIterableWorker(t, use, errorNode) }))
 	}
 	if use&IterationUseAllowsAsyncIterablesFlag != 0 {
 		iterationTypes := c.getIterationTypesOfIterableFast(t, c.asyncIterationTypesResolver)
