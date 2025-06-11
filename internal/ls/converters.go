2ca88065a680ed389b8274336488d7023bf83cd1
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2025-06-11 13:48:47 -0700

    Fix handling of non-file URIs

--- internal/ls/converters.go
+++ internal/ls/converters.go
@@ -81,38 +81,39 @@ func LanguageKindToScriptKind(languageID lsproto.LanguageKind) core.ScriptKind {
 }
 
 func DocumentURIToFileName(uri lsproto.DocumentUri) string {
-	parsed := core.Must(url.Parse(string(uri)))
-	if parsed.Scheme == "file" {
+	if strings.HasPrefix(string(uri), "file://") {
+		parsed := core.Must(url.Parse(string(uri)))
 		if parsed.Host != "" {
 			return "//" + parsed.Host + parsed.Path
 		}
-		return fixWindowsURIPath(parsed.Path)
+		return fixWindowsURIPath(parsed.Path, true)
 	}
 
-	authority := parsed.Host
-	if authority == "" {
-		authority = "ts-nul-authority"
+	// Leave all other URIs escaped so we can round-trip them.
+
+	scheme, path, ok := strings.Cut(string(uri), ":")
+	if !ok {
+		panic(fmt.Sprintf("invalid URI: %s", uri))
 	}
-	path := parsed.Path
-	if path == "" {
-		path = parsed.Opaque
+
+	authority := "ts-nul-authority"
+	if rest, ok := strings.CutPrefix(path, "//"); ok {
+		authority, path, _ = strings.Cut(rest, "/")
 	}
-	if !strings.HasPrefix(path, "/") {
-		path = "/" + path
-	}
-	path = fixWindowsURIPath(path)
-	if !strings.HasPrefix(path, "/") {
-		path = "/" + path
-	}
-	fragment := parsed.Fragment
-	if fragment != "" {
-		fragment = "#" + fragment
-	}
-	return fmt.Sprintf("^/%s/%s%s%s", parsed.Scheme, authority, path, fragment)
+
+	return "^/" + scheme + "/" + authority + "/" + path
 }
 
-func fixWindowsURIPath(path string) string {
-	if rest, ok := strings.CutPrefix(path, "/"); ok {
+func fixWindowsURIPath(path string, cutLeadingSlash bool) string {
+	var rest string
+	var ok bool
+	if cutLeadingSlash {
+		rest, ok = strings.CutPrefix(path, "/")
+	} else {
+		rest = path
+		ok = true
+	}
+	if ok {
 		if volume, rest, ok := splitVolumePath(rest); ok {
 			return volume + rest
 		}
@@ -154,7 +155,12 @@ var extraEscapeReplacer = strings.NewReplacer(
 
 func FileNameToDocumentURI(fileName string) lsproto.DocumentUri {
 	if strings.HasPrefix(fileName, "^/") {
-		return lsproto.DocumentUri(strings.Replace(fileName[2:], "/ts-nul-authority/", ":", 1))
+		scheme, rest, _ := strings.Cut(fileName[2:], "/")
+		authority, path, _ := strings.Cut(rest, "/")
+		if authority == "ts-nul-authority" {
+			return lsproto.DocumentUri(scheme + ":" + path)
+		}
+		return lsproto.DocumentUri(scheme + "://" + authority + "/" + path)
 	}
 
 	volume, fileName, _ := splitVolumePath(fileName)
