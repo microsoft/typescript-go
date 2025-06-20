4831021aff233ea5a56851efde5d00670423be5a
Daniel Rosenwasser <DanielRosenwasser@users.noreply.github.com>
2025-06-20 10:40:50 -0700

    Fix up absolutely wrong test.

--- testdata/tests/cases/compiler/superCallInJSWithWrongBaseTypeArgumentCount1.ts
+++ testdata/tests/cases/compiler/superCallInJSWithWrongBaseTypeArgumentCount1.ts
@@ -1,13 +1,19 @@
-// @module: node20
+// @target: esnext
+// @module: nodenext
+// @checkJs: true
+// @declaration: true
+
 // @filename: a.ts
 export class A<T> {}
 
 // @filename: b.js
-// export class B2 extends A {
-//     constructor() {
-//         super();
-//     }
-// }
+import { A } from './a.js';
+
+export class B1 extends A {
+    constructor() {
+        super();
+    }
+}
 
 export class B2 extends A<unknown, unknown> {
     constructor() {
