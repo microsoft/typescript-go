5bb73c4cba3c19ab9427520fa1abb1052a5acf00
Mateusz Burzyński <mateuszburzynski@gmail.com>
2025-06-24 12:46:20 +0200

    add extra test case

---
+++ testdata/baselines/reference/conformance/jsDeclarationsDefault2.js
@@ -0,0 +1,28 @@
+//// [tests/cases/conformance/jsdoc/declarations/jsDeclarationsDefault2.ts] ////
+
+//// [index1.js]
+export const _default = class {};
+
+export default 12;
+/**
+ * @typedef {string | number} default
+ */
+
+
+//// [index1.js]
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports._default = void 0;
+const _default = class {
+};
+exports._default = _default;
+exports.default = 12;
+
+
+//// [index1.d.ts]
+export declare const _default: {
+    new (): {};
+};
+declare const _default_1: number;
+export default _default_1;
+export type _default_1 = string | number;
