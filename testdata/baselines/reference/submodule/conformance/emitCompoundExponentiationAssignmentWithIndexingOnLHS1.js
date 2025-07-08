47c407f445f2fc5954c092d144007cb1ca0ff5d8
Wesley Wigham <wwigham@gmail.com>
2025-07-08 15:19:22 -0700

    Reintroduce _n skip to remove another 2 baseline diffs

--- testdata/baselines/reference/submodule/conformance/emitCompoundExponentiationAssignmentWithIndexingOnLHS1.js
+++ testdata/baselines/reference/submodule/conformance/emitCompoundExponentiationAssignmentWithIndexingOnLHS1.js
@@ -18,7 +18,7 @@ var j0 = 0, j1 = 1;
 array3[j0++] **= array3[j1++] **= array3[j0++] **= 1;
 
 //// [emitCompoundExponentiationAssignmentWithIndexingOnLHS1.js]
-var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k, _l, _m, _n, _o;
+var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k, _l, _m, _o, _p;
 var array0 = [1, 2, 3];
 var i0 = 0;
 (_a = array0)[_b = ++i0] = Math.pow(_a[_b], 2);
@@ -30,4 +30,4 @@ var i2 = 0;
 (_g = array2)[_h = ++i2] = Math.pow(_g[_h], Math.pow(array2[++i2], 2));
 var array3 = [2, 2, 3];
 var j0 = 0, j1 = 1;
-(_n = array3)[_o = j0++] = Math.pow(_n[_o], (_l = array3)[_m = j1++] = Math.pow(_l[_m], (_j = array3)[_k = j0++] = Math.pow(_j[_k], 1)));
+(_o = array3)[_p = j0++] = Math.pow(_o[_p], (_l = array3)[_m = j1++] = Math.pow(_l[_m], (_j = array3)[_k = j0++] = Math.pow(_j[_k], 1)));
