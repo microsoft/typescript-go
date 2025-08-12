5284b05197b54d7e63e2962e766d95a4b1142be0
Sheetal Nandi <shkamat@microsoft.com>
2025-08-12 01:05:55 -0700

    More concurrency and race issues in baselining fixing

--- internal/testutil/harnessutil/harnessutil.go
+++ internal/testutil/harnessutil/harnessutil.go
@@ -516,10 +516,12 @@ func (h *cachedCompilerHost) GetSourceFile(opts ast.SourceFileParseOptions) *ast
 }
 
 type tracer struct {
-	fs               vfs.FS
-	currentDirectory string
-	packageJsonCache map[tspath.Path]bool
-	builder          strings.Builder
+	fs                         vfs.FS
+	currentDirectory           string
+	packageJsonCache           map[tspath.Path]bool
+	builder                    strings.Builder
+	specifiTypesVersionMessage bool
+	noTypesVersionMessage      bool
 }
 
 func (t *tracer) trace(msg string) {
@@ -527,6 +529,26 @@ func (t *tracer) trace(msg string) {
 }
 
 func (t *tracer) sanitizeTrace(msg string) string {
+	if msg == "'package.json' has a 'typesVersions' field with version-specific path mappings." {
+		t.specifiTypesVersionMessage = true
+		t.noTypesVersionMessage = false
+		return msg
+	}
+	if msg == "'package.json' does not have a 'typesVersions' field." {
+		t.noTypesVersionMessage = true
+		t.specifiTypesVersionMessage = false
+		return msg
+	}
+	if strings.HasPrefix(msg, "'package.json' has a 'typesVersions' entry '") && !t.specifiTypesVersionMessage {
+		fmt.Fprintln(&t.builder, "'package.json' has a 'typesVersions' field with version-specific path mappings.")
+	}
+	if (msg == "'package.json' does not have a 'typings' field." ||
+		strings.HasPrefix(msg, "'package.json' has 'typings' field '")) &&
+		!t.noTypesVersionMessage {
+		fmt.Fprintln(&t.builder, "'package.json' does not have a 'typesVersions' field.")
+	}
+	t.noTypesVersionMessage = false
+	t.specifiTypesVersionMessage = false
 	// Version
 	if str := strings.Replace(msg, "'"+core.Version()+"'", "'"+FakeTSVersion+"'", 1); str != msg {
 		return str
