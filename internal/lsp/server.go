d958cdee3a40b0fea8a31859ef66e4608d3d95e5
Johnny Cheng <johnnyxc@Johnnys-MacBook-Pro.local>
2025-08-27 15:40:29 +1000

    fix panic

--- internal/lsp/server.go
+++ internal/lsp/server.go
@@ -640,13 +640,13 @@ func (s *Server) handleInitialized(ctx context.Context, params *lsproto.Initiali
 
 	s.session = project.NewSession(&project.SessionInit{
 		Options: &project.SessionOptions{
-			CurrentDirectory:   s.cwd,
-			DefaultLibraryPath: s.defaultLibraryPath,
-			TypingsLocation:    s.typingsLocation,
-			PositionEncoding:   s.positionEncoding,
-			WatchEnabled:       s.watchEnabled,
-			LoggingEnabled:     true,
-			DebounceDelay:      500 * time.Millisecond,
+			CurrentDirectory:     s.cwd,
+			DefaultLibraryPath:   s.defaultLibraryPath,
+			TypingsLocation:      s.typingsLocation,
+			PositionEncoding:     s.positionEncoding,
+			WatchEnabled:         s.watchEnabled,
+			LoggingEnabled:       true,
+			DebounceDelay:        500 * time.Millisecond,
 			CustomConfigFileName: customConfigFileName,
 		},
 		FS:          s.fs,
