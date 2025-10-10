9e6e02182fdea19417ac8a6f7c22f1a6f0cd961e
Sheetal Nandi <shkamat@microsoft.com>
2025-10-10 11:59:20 -0700

    Factor out lsp server setup so we can use it for some unit testing

--- internal/fourslash/fourslash.go
+++ internal/fourslash/fourslash.go
@@ -1,16 +1,13 @@
 package fourslash
 
 import (
-	"context"
 	"fmt"
-	"io"
 	"maps"
 	"slices"
 	"strings"
 	"testing"
 	"unicode/utf8"
 
-	"github.com/go-json-experiment/json"
 	"github.com/google/go-cmp/cmp"
 	"github.com/microsoft/typescript-go/internal/bundled"
 	"github.com/microsoft/typescript-go/internal/collections"
@@ -18,25 +15,20 @@ import (
 	"github.com/microsoft/typescript-go/internal/ls"
 	"github.com/microsoft/typescript-go/internal/ls/lsconv"
 	"github.com/microsoft/typescript-go/internal/ls/lsutil"
-	"github.com/microsoft/typescript-go/internal/lsp"
 	"github.com/microsoft/typescript-go/internal/lsp/lsproto"
 	"github.com/microsoft/typescript-go/internal/project"
 	"github.com/microsoft/typescript-go/internal/repo"
 	"github.com/microsoft/typescript-go/internal/stringutil"
 	"github.com/microsoft/typescript-go/internal/testutil/baseline"
 	"github.com/microsoft/typescript-go/internal/testutil/harnessutil"
+	"github.com/microsoft/typescript-go/internal/testutil/lsptestutil"
 	"github.com/microsoft/typescript-go/internal/tspath"
-	"github.com/microsoft/typescript-go/internal/vfs"
 	"github.com/microsoft/typescript-go/internal/vfs/vfstest"
 	"gotest.tools/v3/assert"
 )
 
 type FourslashTest struct {
-	server *lsp.Server
-	in     *lspWriter
-	out    *lspReader
-	id     int32
-	vfs    vfs.FS
+	lsptestutil.TestLspServer
 
 	testData     *TestData // !!! consolidate test files from test data and script info
 	baselines    map[string]*strings.Builder
@@ -45,7 +37,6 @@ type FourslashTest struct {
 	scriptInfos map[string]*scriptInfo
 	converters  *lsconv.Converters
 
-	userPreferences      *lsutil.UserPreferences
 	currentCaretPosition lsproto.Position
 	lastKnownMarkerName  *string
 	activeFilename       string
@@ -82,41 +73,6 @@ func (s *scriptInfo) FileName() string {
 	return s.fileName
 }
 
-type lspReader struct {
-	c <-chan *lsproto.Message
-}
-
-func (r *lspReader) Read() (*lsproto.Message, error) {
-	msg, ok := <-r.c
-	if !ok {
-		return nil, io.EOF
-	}
-	return msg, nil
-}
-
-type lspWriter struct {
-	c chan<- *lsproto.Message
-}
-
-func (w *lspWriter) Write(msg *lsproto.Message) error {
-	w.c <- msg
-	return nil
-}
-
-func (r *lspWriter) Close() {
-	close(r.c)
-}
-
-var (
-	_ lsp.Reader = (*lspReader)(nil)
-	_ lsp.Writer = (*lspWriter)(nil)
-)
-
-func newLSPPipe() (*lspReader, *lspWriter) {
-	c := make(chan *lsproto.Message, 100)
-	return &lspReader{c: c}, &lspWriter{c: c}
-}
-
 const rootDir = "/"
 
 var parseCache = project.ParseCache{
@@ -146,33 +102,8 @@ func NewFourslash(t *testing.T, capabilities *lsproto.ClientCapabilities, conten
 		SkipDefaultLibCheck: core.TSTrue,
 	}
 	harnessutil.SetCompilerOptionsFromTestConfig(t, testData.GlobalOptions, compilerOptions, rootDir)
-
-	inputReader, inputWriter := newLSPPipe()
-	outputReader, outputWriter := newLSPPipe()
 	fs := bundled.WrapFS(vfstest.FromMap(testfs, true /*useCaseSensitiveFileNames*/))
-
-	var err strings.Builder
-	server := lsp.NewServer(&lsp.ServerOptions{
-		In:  inputReader,
-		Out: outputWriter,
-		Err: &err,
-
-		Cwd:                "/",
-		FS:                 fs,
-		DefaultLibraryPath: bundled.LibPath(),
-
-		ParseCache: &parseCache,
-	})
-
-	go func() {
-		defer func() {
-			outputWriter.Close()
-		}()
-		err := server.Run(context.TODO())
-		if err != nil {
-			t.Error("server error:", err)
-		}
-	}()
+	lspTestServer := lsptestutil.NewTestLspServer(t, fs, &parseCache, compilerOptions, capabilities)
 
 	converters := lsconv.NewConverters(lsproto.PositionEncodingKindUTF8, func(fileName string) *lsconv.LSPLineMap {
 		scriptInfo, ok := scriptInfos[fileName]
@@ -183,28 +114,19 @@ func NewFourslash(t *testing.T, capabilities *lsproto.ClientCapabilities, conten
 	})
 
 	f := &FourslashTest{
-		server:          server,
-		in:              inputWriter,
-		out:             outputReader,
-		testData:        &testData,
-		userPreferences: lsutil.NewDefaultUserPreferences(), // !!! parse default preferences for fourslash case?
-		vfs:             fs,
-		scriptInfos:     scriptInfos,
-		converters:      converters,
-		baselines:       make(map[string]*strings.Builder),
+		TestLspServer: *lspTestServer,
+		testData:      &testData,
+		scriptInfos:   scriptInfos,
+		converters:    converters,
+		baselines:     make(map[string]*strings.Builder),
 	}
 
-	// !!! temporary; remove when we have `handleDidChangeConfiguration`/implicit project config support
-	// !!! replace with a proper request *after initialize*
-	f.server.SetCompilerOptionsForInferredProjects(t.Context(), compilerOptions)
-	f.initialize(t, capabilities)
 	for _, file := range testData.Files {
 		f.openFile(t, file.fileName)
 	}
 	f.activeFilename = f.testData.Files[0].fileName
 
 	t.Cleanup(func() {
-		inputWriter.Close()
 		f.verifyBaselines(t)
 	})
 	return f
@@ -215,176 +137,23 @@ func getBaseFileNameFromTest(t *testing.T) string {
 	return stringutil.LowerFirstChar(name)
 }
 
-func (f *FourslashTest) nextID() int32 {
-	id := f.id
-	f.id++
-	return id
-}
-
-func (f *FourslashTest) initialize(t *testing.T, capabilities *lsproto.ClientCapabilities) {
-	params := &lsproto.InitializeParams{
-		Locale: ptrTo("en-US"),
-	}
-	params.Capabilities = getCapabilitiesWithDefaults(capabilities)
-	// !!! check for errors?
-	sendRequest(t, f, lsproto.InitializeInfo, params)
-	sendNotification(t, f, lsproto.InitializedInfo, &lsproto.InitializedParams{})
-}
-
-var (
-	ptrTrue                       = ptrTo(true)
-	defaultCompletionCapabilities = &lsproto.CompletionClientCapabilities{
-		CompletionItem: &lsproto.ClientCompletionItemOptions{
-			SnippetSupport:          ptrTrue,
-			CommitCharactersSupport: ptrTrue,
-			PreselectSupport:        ptrTrue,
-			LabelDetailsSupport:     ptrTrue,
-			InsertReplaceSupport:    ptrTrue,
-			DocumentationFormat:     &[]lsproto.MarkupKind{lsproto.MarkupKindMarkdown, lsproto.MarkupKindPlainText},
-		},
-		CompletionList: &lsproto.CompletionListCapabilities{
-			ItemDefaults: &[]string{"commitCharacters", "editRange"},
-		},
-	}
-	defaultDefinitionCapabilities = &lsproto.DefinitionClientCapabilities{
-		LinkSupport: ptrTrue,
-	}
-	defaultTypeDefinitionCapabilities = &lsproto.TypeDefinitionClientCapabilities{
-		LinkSupport: ptrTrue,
-	}
-	defaultHoverCapabilities = &lsproto.HoverClientCapabilities{
-		ContentFormat: &[]lsproto.MarkupKind{lsproto.MarkupKindMarkdown, lsproto.MarkupKindPlainText},
-	}
-)
-
-func getCapabilitiesWithDefaults(capabilities *lsproto.ClientCapabilities) *lsproto.ClientCapabilities {
-	var capabilitiesWithDefaults lsproto.ClientCapabilities
-	if capabilities != nil {
-		capabilitiesWithDefaults = *capabilities
-	}
-	capabilitiesWithDefaults.General = &lsproto.GeneralClientCapabilities{
-		PositionEncodings: &[]lsproto.PositionEncodingKind{lsproto.PositionEncodingKindUTF8},
-	}
-	if capabilitiesWithDefaults.TextDocument == nil {
-		capabilitiesWithDefaults.TextDocument = &lsproto.TextDocumentClientCapabilities{}
-	}
-	if capabilitiesWithDefaults.TextDocument.Completion == nil {
-		capabilitiesWithDefaults.TextDocument.Completion = defaultCompletionCapabilities
-	}
-	if capabilitiesWithDefaults.TextDocument.Diagnostic == nil {
-		capabilitiesWithDefaults.TextDocument.Diagnostic = &lsproto.DiagnosticClientCapabilities{
-			RelatedInformation: ptrTrue,
-			TagSupport: &lsproto.ClientDiagnosticsTagOptions{
-				ValueSet: []lsproto.DiagnosticTag{
-					lsproto.DiagnosticTagUnnecessary,
-					lsproto.DiagnosticTagDeprecated,
-				},
-			},
-		}
-	}
-	if capabilitiesWithDefaults.Workspace == nil {
-		capabilitiesWithDefaults.Workspace = &lsproto.WorkspaceClientCapabilities{}
-	}
-	if capabilitiesWithDefaults.Workspace.Configuration == nil {
-		capabilitiesWithDefaults.Workspace.Configuration = ptrTrue
-	}
-	if capabilitiesWithDefaults.TextDocument.Definition == nil {
-		capabilitiesWithDefaults.TextDocument.Definition = defaultDefinitionCapabilities
-	}
-	if capabilitiesWithDefaults.TextDocument.TypeDefinition == nil {
-		capabilitiesWithDefaults.TextDocument.TypeDefinition = defaultTypeDefinitionCapabilities
-	}
-	if capabilitiesWithDefaults.TextDocument.Hover == nil {
-		capabilitiesWithDefaults.TextDocument.Hover = defaultHoverCapabilities
-	}
-	if capabilitiesWithDefaults.TextDocument.SignatureHelp == nil {
-		capabilitiesWithDefaults.TextDocument.SignatureHelp = &lsproto.SignatureHelpClientCapabilities{
-			SignatureInformation: &lsproto.ClientSignatureInformationOptions{
-				DocumentationFormat: &[]lsproto.MarkupKind{lsproto.MarkupKindMarkdown, lsproto.MarkupKindPlainText},
-				ParameterInformation: &lsproto.ClientSignatureParameterInformationOptions{
-					LabelOffsetSupport: ptrTrue,
-				},
-				ActiveParameterSupport: ptrTrue,
-			},
-			ContextSupport: ptrTrue,
-		}
-	}
-	return &capabilitiesWithDefaults
-}
-
 func sendRequest[Params, Resp any](t *testing.T, f *FourslashTest, info lsproto.RequestInfo[Params, Resp], params Params) (*lsproto.Message, Resp, bool) {
-	id := f.nextID()
-	req := lsproto.NewRequestMessage(
-		info.Method,
-		lsproto.NewID(lsproto.IntegerOrString{Integer: &id}),
-		params,
-	)
-	f.writeMsg(t, req.Message())
-	resp := f.readMsg(t)
-	if resp == nil {
-		return nil, *new(Resp), false
-	}
-
-	// currently, the only request that may be sent by the server during a client request is one `config` request
-	// !!! remove if `config` is handled in initialization and there are no other server-initiated requests
-	if resp.Kind == lsproto.MessageKindRequest {
-		req := resp.AsRequest()
-		switch req.Method {
-		case lsproto.MethodWorkspaceConfiguration:
-			req := lsproto.ResponseMessage{
-				ID:      req.ID,
-				JSONRPC: req.JSONRPC,
-				Result:  []any{f.userPreferences},
-			}
-			f.writeMsg(t, req.Message())
-			resp = f.readMsg(t)
-		default:
-			// other types of requests not yet used in fourslash; implement them if needed
-			t.Fatalf("Unexpected request received: %s", req.Method)
-		}
-	}
-
-	if resp == nil {
-		return nil, *new(Resp), false
-	}
-	result, ok := resp.AsResponse().Result.(Resp)
-	return resp, result, ok
+	return lsptestutil.SendRequest(t, &f.TestLspServer, info, params)
 }
 
 func sendNotification[Params any](t *testing.T, f *FourslashTest, info lsproto.NotificationInfo[Params], params Params) {
-	notification := lsproto.NewNotificationMessage(
-		info.Method,
-		params,
-	)
-	f.writeMsg(t, notification.Message())
-}
-
-func (f *FourslashTest) writeMsg(t *testing.T, msg *lsproto.Message) {
-	assert.NilError(t, json.MarshalWrite(io.Discard, msg), "failed to encode message as JSON")
-	if err := f.in.Write(msg); err != nil {
-		t.Fatalf("failed to write message: %v", err)
-	}
-}
-
-func (f *FourslashTest) readMsg(t *testing.T) *lsproto.Message {
-	// !!! filter out response by id etc
-	msg, err := f.out.Read()
-	if err != nil {
-		t.Fatalf("failed to read message: %v", err)
-	}
-	assert.NilError(t, json.MarshalWrite(io.Discard, msg), "failed to encode message as JSON")
-	return msg
+	lsptestutil.SendNotification(t, &f.TestLspServer, info, params)
 }
 
 func (f *FourslashTest) Configure(t *testing.T, config *lsutil.UserPreferences) {
-	f.userPreferences = config
+	f.TestLspServer.UserPreferences = config
 	sendNotification(t, f, lsproto.WorkspaceDidChangeConfigurationInfo, &lsproto.DidChangeConfigurationParams{
 		Settings: config,
 	})
 }
 
 func (f *FourslashTest) ConfigureWithReset(t *testing.T, config *lsutil.UserPreferences) (reset func()) {
-	originalConfig := f.userPreferences.Copy()
+	originalConfig := f.TestLspServer.UserPreferences.Copy()
 	f.Configure(t, config)
 	return func() {
 		f.Configure(t, originalConfig)
@@ -1757,7 +1526,7 @@ func (f *FourslashTest) editScript(t *testing.T, fileName string, start int, end
 	}
 
 	script.editContent(start, end, newText)
-	err := f.vfs.WriteFile(fileName, script.content, false)
+	err := f.Vfs.WriteFile(fileName, script.content, false)
 	if err != nil {
 		panic(fmt.Sprintf("Failed to write file %s: %v", fileName, err))
 	}
@@ -1961,7 +1730,7 @@ func (f *FourslashTest) BaselineAutoImportsCompletions(t *testing.T, markerNames
 
 		f.writeToBaseline("Auto Imports", "// === Auto Imports === \n")
 
-		fileContent, ok := f.vfs.ReadFile(f.activeFilename)
+		fileContent, ok := f.Vfs.ReadFile(f.activeFilename)
 		if !ok {
 			t.Fatalf(prefix+"Failed to read file %s for auto-import baseline", f.activeFilename)
 		}
---
+++ internal/testutil/lsptestutil/lsptestutil.go
@@ -0,0 +1,281 @@
+package lsptestutil
+
+import (
+	"context"
+	"io"
+	"strings"
+	"testing"
+
+	"github.com/go-json-experiment/json"
+	"github.com/microsoft/typescript-go/internal/bundled"
+	"github.com/microsoft/typescript-go/internal/core"
+	"github.com/microsoft/typescript-go/internal/ls/lsutil"
+	"github.com/microsoft/typescript-go/internal/lsp"
+	"github.com/microsoft/typescript-go/internal/lsp/lsproto"
+	"github.com/microsoft/typescript-go/internal/project"
+	"github.com/microsoft/typescript-go/internal/vfs"
+	"gotest.tools/v3/assert"
+)
+
+type lspReader struct {
+	c <-chan *lsproto.Message
+}
+
+func (r *lspReader) Read() (*lsproto.Message, error) {
+	msg, ok := <-r.c
+	if !ok {
+		return nil, io.EOF
+	}
+	return msg, nil
+}
+
+type lspWriter struct {
+	c chan<- *lsproto.Message
+}
+
+func (w *lspWriter) Write(msg *lsproto.Message) error {
+	w.c <- msg
+	return nil
+}
+
+func (r *lspWriter) Close() {
+	close(r.c)
+}
+
+var (
+	_ lsp.Reader = (*lspReader)(nil)
+	_ lsp.Writer = (*lspWriter)(nil)
+)
+
+func newLSPPipe() (*lspReader, *lspWriter) {
+	c := make(chan *lsproto.Message, 100)
+	return &lspReader{c: c}, &lspWriter{c: c}
+}
+
+var (
+	ptrTrue                       = ptrTo(true)
+	defaultCompletionCapabilities = &lsproto.CompletionClientCapabilities{
+		CompletionItem: &lsproto.ClientCompletionItemOptions{
+			SnippetSupport:          ptrTrue,
+			CommitCharactersSupport: ptrTrue,
+			PreselectSupport:        ptrTrue,
+			LabelDetailsSupport:     ptrTrue,
+			InsertReplaceSupport:    ptrTrue,
+			DocumentationFormat:     &[]lsproto.MarkupKind{lsproto.MarkupKindMarkdown, lsproto.MarkupKindPlainText},
+		},
+		CompletionList: &lsproto.CompletionListCapabilities{
+			ItemDefaults: &[]string{"commitCharacters", "editRange"},
+		},
+	}
+	defaultDefinitionCapabilities = &lsproto.DefinitionClientCapabilities{
+		LinkSupport: ptrTrue,
+	}
+	defaultTypeDefinitionCapabilities = &lsproto.TypeDefinitionClientCapabilities{
+		LinkSupport: ptrTrue,
+	}
+	defaultHoverCapabilities = &lsproto.HoverClientCapabilities{
+		ContentFormat: &[]lsproto.MarkupKind{lsproto.MarkupKindMarkdown, lsproto.MarkupKindPlainText},
+	}
+)
+
+type TestLspServer struct {
+	Server          *lsp.Server
+	in              *lspWriter
+	out             *lspReader
+	id              int32
+	Vfs             vfs.FS
+	UserPreferences *lsutil.UserPreferences
+}
+
+func NewTestLspServer(
+	t *testing.T,
+	fs vfs.FS,
+	parseCache *project.ParseCache,
+	optionsForInferredProject *core.CompilerOptions,
+	capabilities *lsproto.ClientCapabilities,
+) *TestLspServer {
+	t.Helper()
+
+	inputReader, inputWriter := newLSPPipe()
+	outputReader, outputWriter := newLSPPipe()
+
+	var err strings.Builder
+	server := lsp.NewServer(&lsp.ServerOptions{
+		In:  inputReader,
+		Out: outputWriter,
+		Err: &err,
+
+		Cwd:                "/",
+		FS:                 fs,
+		DefaultLibraryPath: bundled.LibPath(),
+
+		ParseCache: parseCache,
+	})
+
+	go func() {
+		defer func() {
+			outputWriter.Close()
+		}()
+		err := server.Run(context.TODO())
+		if err != nil {
+			t.Error("server error:", err)
+		}
+	}()
+
+	s := &TestLspServer{
+		Server:          server,
+		in:              inputWriter,
+		out:             outputReader,
+		Vfs:             fs,
+		UserPreferences: lsutil.NewDefaultUserPreferences(), // !!! parse default preferences for fourslash case?
+	}
+
+	// !!! temporary; remove when we have `handleDidChangeConfiguration`/implicit project config support
+	// !!! replace with a proper request *after initialize*
+	s.Server.SetCompilerOptionsForInferredProjects(t.Context(), optionsForInferredProject)
+	s.initialize(t, capabilities)
+
+	t.Cleanup(func() {
+		inputWriter.Close()
+	})
+	return s
+}
+
+func (s *TestLspServer) nextID() int32 {
+	id := s.id
+	s.id++
+	return id
+}
+
+func (s *TestLspServer) initialize(t *testing.T, capabilities *lsproto.ClientCapabilities) {
+	params := &lsproto.InitializeParams{
+		Locale: ptrTo("en-US"),
+	}
+	params.Capabilities = getCapabilitiesWithDefaults(capabilities)
+	// !!! check for errors?
+	SendRequest(t, s, lsproto.InitializeInfo, params)
+	SendNotification(t, s, lsproto.InitializedInfo, &lsproto.InitializedParams{})
+}
+
+func getCapabilitiesWithDefaults(capabilities *lsproto.ClientCapabilities) *lsproto.ClientCapabilities {
+	var capabilitiesWithDefaults lsproto.ClientCapabilities
+	if capabilities != nil {
+		capabilitiesWithDefaults = *capabilities
+	}
+	capabilitiesWithDefaults.General = &lsproto.GeneralClientCapabilities{
+		PositionEncodings: &[]lsproto.PositionEncodingKind{lsproto.PositionEncodingKindUTF8},
+	}
+	if capabilitiesWithDefaults.TextDocument == nil {
+		capabilitiesWithDefaults.TextDocument = &lsproto.TextDocumentClientCapabilities{}
+	}
+	if capabilitiesWithDefaults.TextDocument.Completion == nil {
+		capabilitiesWithDefaults.TextDocument.Completion = defaultCompletionCapabilities
+	}
+	if capabilitiesWithDefaults.TextDocument.Diagnostic == nil {
+		capabilitiesWithDefaults.TextDocument.Diagnostic = &lsproto.DiagnosticClientCapabilities{
+			RelatedInformation: ptrTrue,
+			TagSupport: &lsproto.ClientDiagnosticsTagOptions{
+				ValueSet: []lsproto.DiagnosticTag{
+					lsproto.DiagnosticTagUnnecessary,
+					lsproto.DiagnosticTagDeprecated,
+				},
+			},
+		}
+	}
+	if capabilitiesWithDefaults.Workspace == nil {
+		capabilitiesWithDefaults.Workspace = &lsproto.WorkspaceClientCapabilities{}
+	}
+	if capabilitiesWithDefaults.Workspace.Configuration == nil {
+		capabilitiesWithDefaults.Workspace.Configuration = ptrTrue
+	}
+	if capabilitiesWithDefaults.TextDocument.Definition == nil {
+		capabilitiesWithDefaults.TextDocument.Definition = defaultDefinitionCapabilities
+	}
+	if capabilitiesWithDefaults.TextDocument.TypeDefinition == nil {
+		capabilitiesWithDefaults.TextDocument.TypeDefinition = defaultTypeDefinitionCapabilities
+	}
+	if capabilitiesWithDefaults.TextDocument.Hover == nil {
+		capabilitiesWithDefaults.TextDocument.Hover = defaultHoverCapabilities
+	}
+	if capabilitiesWithDefaults.TextDocument.SignatureHelp == nil {
+		capabilitiesWithDefaults.TextDocument.SignatureHelp = &lsproto.SignatureHelpClientCapabilities{
+			SignatureInformation: &lsproto.ClientSignatureInformationOptions{
+				DocumentationFormat: &[]lsproto.MarkupKind{lsproto.MarkupKindMarkdown, lsproto.MarkupKindPlainText},
+				ParameterInformation: &lsproto.ClientSignatureParameterInformationOptions{
+					LabelOffsetSupport: ptrTrue,
+				},
+				ActiveParameterSupport: ptrTrue,
+			},
+			ContextSupport: ptrTrue,
+		}
+	}
+	return &capabilitiesWithDefaults
+}
+
+func SendRequest[Params, Resp any](t *testing.T, server *TestLspServer, info lsproto.RequestInfo[Params, Resp], params Params) (*lsproto.Message, Resp, bool) {
+	id := server.nextID()
+	req := lsproto.NewRequestMessage(
+		info.Method,
+		lsproto.NewID(lsproto.IntegerOrString{Integer: &id}),
+		params,
+	)
+	server.writeMsg(t, req.Message())
+	resp := server.readMsg(t)
+	if resp == nil {
+		return nil, *new(Resp), false
+	}
+
+	// currently, the only request that may be sent by the server during a client request is one `config` request
+	// !!! remove if `config` is handled in initialization and there are no other server-initiated requests
+	if resp.Kind == lsproto.MessageKindRequest {
+		req := resp.AsRequest()
+		switch req.Method {
+		case lsproto.MethodWorkspaceConfiguration:
+			req := lsproto.ResponseMessage{
+				ID:      req.ID,
+				JSONRPC: req.JSONRPC,
+				Result:  []any{server.UserPreferences},
+			}
+			server.writeMsg(t, req.Message())
+			resp = server.readMsg(t)
+		default:
+			// other types of requests not yet used in fourslash; implement them if needed
+			t.Fatalf("Unexpected request received: %s", req.Method)
+		}
+	}
+
+	if resp == nil {
+		return nil, *new(Resp), false
+	}
+	result, ok := resp.AsResponse().Result.(Resp)
+	return resp, result, ok
+}
+
+func SendNotification[Params any](t *testing.T, server *TestLspServer, info lsproto.NotificationInfo[Params], params Params) {
+	notification := lsproto.NewNotificationMessage(
+		info.Method,
+		params,
+	)
+	server.writeMsg(t, notification.Message())
+}
+
+func (s *TestLspServer) writeMsg(t *testing.T, msg *lsproto.Message) {
+	assert.NilError(t, json.MarshalWrite(io.Discard, msg), "failed to encode message as JSON")
+	if err := s.in.Write(msg); err != nil {
+		t.Fatalf("failed to write message: %v", err)
+	}
+}
+
+func (s *TestLspServer) readMsg(t *testing.T) *lsproto.Message {
+	// !!! filter out response by id etc
+	msg, err := s.out.Read()
+	if err != nil {
+		t.Fatalf("failed to read message: %v", err)
+	}
+	assert.NilError(t, json.MarshalWrite(io.Discard, msg), "failed to encode message as JSON")
+	return msg
+}
+
+func ptrTo[T any](v T) *T {
+	return &v
+}
