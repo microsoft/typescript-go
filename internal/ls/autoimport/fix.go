50be94c578feefe38f459358eb0a85636ec2ecb3
Andrew Branch <andrew@wheream.io>
2025-10-29 12:45:12 -0700

    WIP fix edits

--- internal/ls/autoimport/fix.go
+++ internal/ls/autoimport/fix.go
@@ -2,12 +2,18 @@ package autoimport
 
 import (
 	"context"
+	"slices"
 
 	"github.com/microsoft/typescript-go/internal/ast"
+	"github.com/microsoft/typescript-go/internal/astnav"
 	"github.com/microsoft/typescript-go/internal/checker"
 	"github.com/microsoft/typescript-go/internal/collections"
 	"github.com/microsoft/typescript-go/internal/compiler"
 	"github.com/microsoft/typescript-go/internal/core"
+	"github.com/microsoft/typescript-go/internal/debug"
+	"github.com/microsoft/typescript-go/internal/format"
+	"github.com/microsoft/typescript-go/internal/ls/change"
+	"github.com/microsoft/typescript-go/internal/ls/lsconv"
 	"github.com/microsoft/typescript-go/internal/lsp/lsproto"
 	"github.com/microsoft/typescript-go/internal/modulespecifiers"
 )
@@ -32,8 +38,15 @@ const (
 	FixKindPromoteTypeOnly FixKind = 4
 )
 
+type newImportBinding struct {
+	kind         ImportKind
+	propertyName string
+	name         string
+}
+
 type Fix struct {
 	Kind       FixKind    `json:"kind"`
+	Name       string     `json:"name,omitempty"`
 	ImportKind ImportKind `json:"importKind"`
 
 	// FixKindAddNew
@@ -46,8 +59,99 @@ type Fix struct {
 	ImportIndex int `json:"importIndex"`
 }
 
-func (f *Fix) Edits(ctx context.Context, file *ast.SourceFile) []*lsproto.TextEdit {
-	return nil
+func (f *Fix) Edits(ctx context.Context, file *ast.SourceFile, compilerOptions *core.CompilerOptions, formatOptions *format.FormatCodeSettings, converters *lsconv.Converters) []*lsproto.TextEdit {
+	tracker := change.NewTracker(ctx, compilerOptions, formatOptions, converters)
+	switch f.Kind {
+	case FixKindAddToExisting:
+		if len(file.Imports()) <= f.ImportIndex {
+			panic("import index out of range")
+		}
+		moduleSpecifier := file.Imports()[f.ImportIndex]
+		importDecl := ast.TryGetImportFromModuleSpecifier(moduleSpecifier)
+		if importDecl == nil {
+			panic("expected import declaration")
+		}
+		var importClauseOrBindingPattern *ast.Node
+		if importDecl.Kind == ast.KindImportDeclaration {
+			importClauseOrBindingPattern = ast.GetImportClauseOfDeclaration(importDecl).AsNode()
+			if importClauseOrBindingPattern == nil {
+				panic("expected import clause")
+			}
+		} else if importDecl.Kind == ast.KindVariableDeclaration {
+			importClauseOrBindingPattern = importDecl.Name().AsBindingPattern().AsNode()
+		} else {
+			panic("expected import declaration or variable declaration")
+		}
+
+		defaultImport := core.IfElse(f.ImportKind == ImportKindDefault, &newImportBinding{kind: ImportKindDefault, name: f.Name}, nil)
+		namedImports := core.IfElse(f.ImportKind == ImportKindNamed, []*newImportBinding{{kind: ImportKindNamed, name: f.Name}}, nil)
+		addToExistingImport(tracker, file, importClauseOrBindingPattern, defaultImport, namedImports)
+		return tracker.GetChanges()[file.FileName()]
+	default:
+		panic("unimplemented fix edit")
+	}
+}
+
+func addToExistingImport(
+	ct *change.Tracker,
+	file *ast.SourceFile,
+	importClauseOrBindingPattern *ast.Node,
+	defaultImport *newImportBinding,
+	namedImports []*newImportBinding,
+) {
+
+	switch importClauseOrBindingPattern.Kind {
+	case ast.KindObjectBindingPattern:
+		bindingPattern := importClauseOrBindingPattern.AsBindingPattern()
+		if defaultImport != nil {
+			addElementToBindingPattern(ct, file, bindingPattern, defaultImport.name, "default")
+		}
+		for _, namedImport := range namedImports {
+			addElementToBindingPattern(ct, file, bindingPattern, namedImport.name, "")
+		}
+		return
+	case ast.KindImportClause:
+		importClause := importClauseOrBindingPattern.AsImportClause()
+		namedBindings := importClause.NamedBindings
+		if namedBindings == nil || namedBindings.Kind != ast.KindNamedImports {
+			panic("expected named imports")
+		}
+		if defaultImport != nil {
+			debug.Assert(importClause.Name() == nil, "Cannot add a default import to an import clause that already has one")
+			ct.InsertNodeAt(file, core.TextPos(astnav.GetStartOfNode(importClause.AsNode(), file, false)), ct.NodeFactory.NewIdentifier(defaultImport.name), change.NodeOptions{Suffix: ", "})
+		}
+
+		if len(namedImports) > 0 {
+			specifierComparer, isSorted := ls.getNamedImportSpecifierComparerWithDetection(importClause.Parent, file)
+			newSpecifiers := core.Map(namedImports, func(namedImport *newImportBinding) *ast.Node {
+				var identifier *ast.Node
+				if namedImport.propertyName != "" {
+					identifier = ct.NodeFactory.NewIdentifier(namedImport.propertyName).AsIdentifier().AsNode()
+				}
+				return ct.NodeFactory.NewImportSpecifier(
+					false,
+					identifier,
+					ct.NodeFactory.NewIdentifier(namedImport.name),
+				)
+			})
+			slices.SortFunc(newSpecifiers, specifierComparer)
+		}
+	}
+}
+
+func addElementToBindingPattern(
+	ct *change.Tracker,
+	file *ast.SourceFile,
+	bindingPattern *ast.BindingPattern,
+	name string,
+	propertyName string,
+) {
+	element := ct.NodeFactory.NewBindingElement(nil, nil, ct.NodeFactory.NewIdentifier(name), core.IfElse(propertyName == "", nil, ct.NodeFactory.NewIdentifier(propertyName)))
+	if len(bindingPattern.Elements.Nodes) > 0 {
+		ct.InsertNodeInListAfter(file, bindingPattern.Elements.Nodes[len(bindingPattern.Elements.Nodes)-1], element, bindingPattern.Elements.Nodes)
+	} else {
+		ct.ReplaceNode(file, bindingPattern.AsNode(), ct.NodeFactory.NewBindingPattern(ast.KindObjectBindingPattern, ct.AsNodeFactory().NewNodeList([]*ast.Node{element})), nil)
+	}
 }
 
 func GetFixes(
@@ -105,6 +209,7 @@ func tryAddToExistingImport(
 			if (importKind == ImportKindNamed || importKind == ImportKindDefault) && existingImport.node.Name().Kind == ast.KindObjectBindingPattern {
 				return &Fix{
 					Kind:            FixKindAddToExisting,
+					Name:            export.Name,
 					ImportKind:      importKind,
 					ImportIndex:     existingImport.index,
 					ModuleSpecifier: existingImport.moduleSpecifier,
@@ -132,6 +237,7 @@ func tryAddToExistingImport(
 
 		return &Fix{
 			Kind:            FixKindAddToExisting,
+			Name:            export.Name,
 			ImportKind:      importKind,
 			ImportIndex:     existingImport.index,
 			ModuleSpecifier: existingImport.moduleSpecifier,
