2be521c1e36fd4bc3e64c4b27e07b79b25a86459
Sheetal Nandi <shkamat@microsoft.com>
2025-10-30 11:33:14 -0700

    Port special localness checks to determine if we need to load project tree

--- testdata/baselines/reference/lspservertests/findAllRefs/special-handling-of-localness-when-using-arrow-function-as-object-literal-property-types.js
+++ testdata/baselines/reference/lspservertests/findAllRefs/special-handling-of-localness-when-using-arrow-function-as-object-literal-property-types.js
@@ -98,22 +98,39 @@ Projects::
   [/user/username/projects/solution/api/tsconfig.json] 
     /user/username/projects/solution/shared/src/index.ts  
     /user/username/projects/solution/api/src/server.ts    
+  [/user/username/projects/solution/app/tsconfig.json] *new*
+    /user/username/projects/solution/shared/src/index.ts  
+    /user/username/projects/solution/app/src/app.ts       
   [/user/username/projects/solution/shared/tsconfig.json] *new*
     /user/username/projects/solution/shared/src/index.ts  
-  [/user/username/projects/solution/tsconfig.json] 
+  [/user/username/projects/solution/tsconfig.json] *modified*
 Config::
-  [/user/username/projects/solution/api/tsconfig.json] 
-    RetainingProjects:
+  [/user/username/projects/solution/api/tsconfig.json] *modified*
+    RetainingProjects: *modified*
       /user/username/projects/solution/api/tsconfig.json  
+      /user/username/projects/solution/tsconfig.json      *new*
     RetainingOpenFiles:
       /user/username/projects/solution/api/src/server.ts  
+  [/user/username/projects/solution/app/tsconfig.json] *new*
+    RetainingProjects:
+      /user/username/projects/solution/app/tsconfig.json  
+      /user/username/projects/solution/tsconfig.json      
   [/user/username/projects/solution/shared/tsconfig.json] *modified*
     RetainingProjects: *modified*
       /user/username/projects/solution/api/tsconfig.json     
+      /user/username/projects/solution/app/tsconfig.json     *new*
       /user/username/projects/solution/shared/tsconfig.json  *new*
+      /user/username/projects/solution/tsconfig.json         *new*
+  [/user/username/projects/solution/tsconfig.json] *new*
+    RetainingProjects:
+      /user/username/projects/solution/tsconfig.json  
 // === /user/username/projects/solution/api/src/server.ts ===
 // import * as shared from "../../shared/dist"
 // shared.foo./*FIND ALL REFS*/[|bar|]();
+
+// === /user/username/projects/solution/app/src/app.ts ===
+// import * as shared from "../../shared/dist"
+// shared.foo.[|bar|]();
 
 // === /user/username/projects/solution/shared/src/index.ts ===
 // export const foo = { [|bar|]: () => { } };
