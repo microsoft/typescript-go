4e98df1a6a8b9057958bfade0963ef2f04a3174a
Anders Hejlsberg <andersh@microsoft.com>
2025-11-02 11:46:20 -0800

    Add regression test

---
+++ testdata/baselines/reference/compiler/typedefHoisting.js
@@ -0,0 +1,19 @@
+//// [tests/cases/compiler/typedefHoisting.ts] ////
+
+//// [x.js]
+class C {
+    /** @typedef {string} Foo */
+    /** @type {Foo} */
+    foo = "abc"
+}
+
+
+
+
+//// [x.d.ts]
+type Foo = string;
+declare class C {
+    /** @typedef {string} Foo */
+    /** @type {Foo} */
+    foo: Foo;
+}
---
+++ testdata/tests/cases/compiler/typedefHoisting.ts
@@ -0,0 +1,9 @@
+// @allowJs: true
+// @checkJs: true
+// @declaration: true
+// @Filename: x.js
+class C {
+    /** @typedef {string} Foo */
+    /** @type {Foo} */
+    foo = "abc"
+}
