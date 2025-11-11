036536b9f7cf7de2829f5945d9caa4cb0304dba4
haoqixu <hq.xu0o0@gmail.com>
2025-11-11 19:48:01 +0800

    fix syncmap panic on nil

---
+++ internal/collections/syncmap_test.go
@@ -0,0 +1,30 @@
+package collections_test
+
+import (
+	"testing"
+
+	"github.com/microsoft/typescript-go/internal/collections"
+	"gotest.tools/v3/assert"
+)
+
+func TestSyncMapWithNil(t *testing.T) {
+	var m collections.SyncMap[string, any]
+
+	got1, ok := m.Load("foo")
+	assert.Assert(t, !ok)
+	assert.Equal(t, got1, nil)
+
+	m.Store("foo", nil)
+
+	got2, ok := m.Load("foo")
+	assert.Assert(t, ok)
+	assert.Equal(t, got2, nil)
+
+	too, loaded := m.LoadOrStore("too", nil)
+	assert.Assert(t, !loaded)
+	assert.Equal(t, too, nil)
+
+	m.Range(func(k string, v any) bool {
+		return true
+	})
+}
