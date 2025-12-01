4992159278d0223ab1e73548a3a35607f65088d8
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2025-11-30 20:20:33 -0800

    manual some

--- internal/fourslash/tests/manual/jsDocFunctionSignatures2_test.go
+++ internal/fourslash/tests/manual/jsDocFunctionSignatures2_test.go
@@ -9,15 +9,15 @@ import (
 
 func TestJsDocFunctionSignatures2(t *testing.T) {
 	t.Parallel()
-	t.Skip()
+
 	defer testutil.RecoverAndFail(t, "Panic on fourslash test")
 	const content = `// @allowNonTsExtensions: true
 // @Filename: Foo.js
-/** @type {function(string, boolean=): number} */
+/** @type {(arg0: string, arg1?: boolean) => number} */
 var f6;
 
 f6('', /**/false)`
 	f := fourslash.NewFourslash(t, nil /*capabilities*/, content)
 	f.GoToMarker(t, "")
-	f.VerifySignatureHelp(t, fourslash.VerifySignatureHelpOptions{Text: "f6(arg0: string, arg1?: boolean | undefined): number"})
+	f.VerifySignatureHelp(t, fourslash.VerifySignatureHelpOptions{Text: "f6(arg0: string, arg1?: boolean): number"})
 }
