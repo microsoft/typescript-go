727957b27e6249615d4d16dbccb2d2fb9e8e7434
Daniel Rosenwasser <DanielRosenwasser@users.noreply.github.com>
2025-12-01 18:55:40 +0000

    Fix lint issues.

--- internal/fourslash/tests/codeLensShowOnAllFunctions_test.go
+++ internal/fourslash/tests/codeLensShowOnAllFunctions_test.go
@@ -11,10 +11,10 @@ import (
 
 func TestCodeLensReferencesShowOnAllFunctions(t *testing.T) {
 	t.Parallel()
-
 	containingTestName := t.Name()
 	for _, value := range []bool{true, false} {
 		t.Run(fmt.Sprintf("%s=%v", containingTestName, value), func(t *testing.T) {
+			t.Parallel()
 			defer testutil.RecoverAndFail(t, "Panic on fourslash test")
 
 			const content = `
