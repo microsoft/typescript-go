eca5f0103d0048f33736f6547a39a35c26f5cdd2
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2025-12-03 09:25:17 -0800

    Fix test

--- internal/fourslash/tests/goToDefinitionShorthandObjectLiteralWithInterface_test.go
+++ internal/fourslash/tests/goToDefinitionShorthandObjectLiteralWithInterface_test.go
@@ -18,6 +18,7 @@ func TestGoToDefinitionShorthandObjectLiteralWithInterface(t *testing.T) {
 function makeSomething([|foo|]: string): Something {
     return { [|f/*1*/oo|] };
 }`
-	f := fourslash.NewFourslash(t, nil /*capabilities*/, content)
+	f, done := fourslash.NewFourslash(t, nil /*capabilities*/, content)
+	defer done()
 	f.VerifyBaselineGoToDefinition(t, true, "1")
 }
