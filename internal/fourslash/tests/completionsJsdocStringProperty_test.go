031ae5f16144c36cf81d3b5f6c08bc9b1e97d7ed
James Sheppard <jgsheppard92@gmail.com>
2025-12-11 14:07:41 +0100

    test: Write test for string property crash

---
+++ internal/fourslash/tests/completionsJsdocStringProperty_test.go
@@ -0,0 +1,34 @@
+package fourslash_test
+
+import (
+	"testing"
+
+	"github.com/microsoft/typescript-go/internal/fourslash"
+	. "github.com/microsoft/typescript-go/internal/fourslash/tests/util"
+	"github.com/microsoft/typescript-go/internal/testutil"
+)
+
+func TestCompletionsJsdocStringProperty(t *testing.T) {
+	t.Parallel()
+	defer testutil.RecoverAndFail(t, "Panic on fourslash test")
+
+	const content = `/**
+ * @type {{
+ * 'string-property': boolean;
+ */*$*/ identifierProperty: boolean;
+ * }}
+ */
+var someVariable;`
+
+	f, done := fourslash.NewFourslash(t, nil /*capabilities*/, content)
+	defer done()
+	f.GoToMarker(t, "$")
+	f.VerifyCompletions(t, nil, &fourslash.CompletionsExpectedList{
+		IsIncomplete: false,
+		ItemDefaults: &fourslash.CompletionsExpectedItemDefaults{
+			CommitCharacters: &[]string{},
+			EditRange:        Ignored,
+		},
+		Items: &fourslash.CompletionsExpectedItems{},
+	})
+}
