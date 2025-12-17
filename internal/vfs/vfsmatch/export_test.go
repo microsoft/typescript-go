97046feed555827be720e689f36f1c8a98a8f3b3
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2025-12-17 10:06:07 -0800

    fmt

--- internal/vfs/vfsmatch/export_test.go
+++ internal/vfs/vfsmatch/export_test.go
@@ -2,9 +2,8 @@ package vfsmatch
 
 import "github.com/microsoft/typescript-go/internal/vfs"
 
-
 func ReadDirectoryNew(host vfs.FS, currentDir string, path string, extensions []string, excludes []string, includes []string, depth *int) []string {
-		return matchFilesNoRegex(path, extensions, excludes, includes, host.UseCaseSensitiveFileNames(), currentDir, depth, host)
+	return matchFilesNoRegex(path, extensions, excludes, includes, host.UseCaseSensitiveFileNames(), currentDir, depth, host)
 }
 
 func ReadDirectoryOld(host vfs.FS, currentDir string, path string, extensions []string, excludes []string, includes []string, depth *int) []string {
