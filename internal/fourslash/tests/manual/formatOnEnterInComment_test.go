de12c6c5ff4d2a88638064dd4156df82b97be447
Oleksandr Tarasiuk <oleksandr.tarasiuk@outlook.com>
2026-01-13 00:57:12 +0200

    update tests to align with Strada behavior

--- internal/fourslash/tests/manual/formatOnEnterInComment_test.go
+++ internal/fourslash/tests/manual/formatOnEnterInComment_test.go
@@ -7,14 +7,16 @@ import (
 	"github.com/microsoft/typescript-go/internal/testutil"
 )
 
-func TestSemicolonFormattingInsideAComment(t *testing.T) {
+func TestFormatOnEnterInComment(t *testing.T) {
 	fourslash.SkipIfFailing(t)
 	t.Parallel()
 	defer testutil.RecoverAndFail(t, "Panic on fourslash test")
-	const content = `   ///**/`
+	const content = `   /**
+    * /*1*/
+    */`
 	f, done := fourslash.NewFourslash(t, nil /*capabilities*/, content)
 	defer done()
-	f.GoToMarker(t, "")
-	f.Insert(t, ";")
-	f.VerifyCurrentLineContentIs(t, "   //;")
+	f.GoToMarker(t, "1")
+	f.InsertLine(t, "")
+	f.VerifyCurrentFileContentIs(t, "  /**\n   * \n\n   */")
 }
--- internal/fourslash/tests/manual/semicolonFormattingInsideAComment_test.go
+++ internal/fourslash/tests/manual/semicolonFormattingInsideAComment_test.go
@@ -7,14 +7,14 @@ import (
 	"github.com/microsoft/typescript-go/internal/testutil"
 )
 
-func TestSemicolonFormattingInsideAStringLiteral(t *testing.T) {
+func TestSemicolonFormattingInsideAComment(t *testing.T) {
 	fourslash.SkipIfFailing(t)
 	t.Parallel()
 	defer testutil.RecoverAndFail(t, "Panic on fourslash test")
-	const content = `   var x = "string/**/`
+	const content = `    ///**/`
 	f, done := fourslash.NewFourslash(t, nil /*capabilities*/, content)
 	defer done()
 	f.GoToMarker(t, "")
 	f.Insert(t, ";")
-	f.VerifyCurrentLineContentIs(t, "   var x = \"string;")
+	f.VerifyCurrentLineContentIs(t, "   //;")
 }
