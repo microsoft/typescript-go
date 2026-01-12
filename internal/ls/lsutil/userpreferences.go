bba802b1bba82c52d54587c22800b6667172c18d
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2026-01-12 11:25:20 -0800

    some fixes

--- internal/ls/lsutil/userpreferences.go
+++ internal/ls/lsutil/userpreferences.go
@@ -746,8 +746,5 @@ func (p *UserPreferences) ParsedAutoImportFileExcludePatterns(useCaseSensitiveFi
 }
 
 func (p *UserPreferences) IsModuleSpecifierExcluded(moduleSpecifier string) bool {
-	if modulespecifiers.IsExcludedByRegex(moduleSpecifier, p.AutoImportSpecifierExcludeRegexes) {
-		return true
-	}
-	return false
+	return modulespecifiers.IsExcludedByRegex(moduleSpecifier, p.AutoImportSpecifierExcludeRegexes)
 }
