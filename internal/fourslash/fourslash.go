8614acba41b1cf1ceeaf66aaeab331748985cf78
Sebastian Lorenz <fubhy@fubhy.com>
2026-01-15 14:41:01 +0100

    Use thread safe writer in tests

--- internal/fourslash/fourslash.go
+++ internal/fourslash/fourslash.go
@@ -206,11 +206,10 @@ func NewFourslash(t *testing.T, capabilities *lsproto.ClientCapabilities, conten
 	fsFromMap := vfstest.FromMap(testfs, true /*useCaseSensitiveFileNames*/)
 	fs := bundled.WrapFS(fsFromMap)
 
-	var err strings.Builder
 	server := lsp.NewServer(&lsp.ServerOptions{
 		In:  inputReader,
 		Out: outputWriter,
-		Err: &err,
+		Err: t.Output(),
 
 		Cwd:                "/",
 		FS:                 fs,
