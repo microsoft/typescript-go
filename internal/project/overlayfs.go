17b186d0d026f5ac136c807a937a258205c5a01d
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2026-01-16 09:35:17 -0800

    Compare contents on reopen

--- internal/project/overlayfs.go
+++ internal/project/overlayfs.go
@@ -303,6 +303,9 @@ func (fs *overlayFS) processChanges(changes []FileChange) (FileChangeSummary, ma
 			if result.Opened != "" {
 				panic("can only process one file open event at a time")
 			}
+			if events.reopened && o != nil && o.Content() == events.openChange.Content {
+				continue
+			}
 			result.Opened = uri
 			if events.reopened {
 				result.Changed.Add(uri)
