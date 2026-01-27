0ecf95b3f14ac8b96b680c612e1fe8db66c5861f
Isabel Duan <isabelduan@microsoft.com>
2026-01-27 14:34:08 -0800

    formatSelection tests + extra whitespace bug fix

---
+++ internal/fourslash/tests/gen/formatSelectionWithTrivia6_test.go
@@ -0,0 +1,21 @@
+package fourslash_test
+
+import (
+	"testing"
+
+	"github.com/microsoft/typescript-go/internal/fourslash"
+	"github.com/microsoft/typescript-go/internal/testutil"
+)
+
+func TestFormatSelectionWithTrivia6(t *testing.T) {
+	fourslash.SkipIfFailing(t)
+	t.Parallel()
+	defer testutil.RecoverAndFail(t, "Panic on fourslash test")
+	const content = `/*begin*/    // test comment
+/*end*/`
+	f, done := fourslash.NewFourslash(t, nil /*capabilities*/, content)
+	defer done()
+	f.FormatSelection(t, "begin", "end")
+	f.VerifyCurrentFileContent(t, `// test comment
+`)
+}
---
+++ internal/fourslash/tests/gen/formatV8Directive_test.go
@@ -0,0 +1,20 @@
+package fourslash_test
+
+import (
+	"testing"
+
+	"github.com/microsoft/typescript-go/internal/fourslash"
+	"github.com/microsoft/typescript-go/internal/testutil"
+)
+
+func TestFormatV8Directive(t *testing.T) {
+	fourslash.SkipIfFailing(t)
+	t.Parallel()
+	defer testutil.RecoverAndFail(t, "Panic on fourslash test")
+	const content = `// @Filename: foo.js
+function foo() {}
+/*1*/%PrepareFunctionForOptimization(foo)/*2*/;`
+	f, done := fourslash.NewFourslash(t, nil /*capabilities*/, content)
+	defer done()
+	f.FormatSelection(t, "1", "2")
+}
