1ca8acf3442091a6b0a653aa58faa51dc77d7c4e
Anders Hejlsberg <andersh@microsoft.com>
2026-01-31 17:06:27 -1000

    Fix formatting

--- internal/fourslash/tests/manual/augmentedTypesModule2_test.go
+++ internal/fourslash/tests/manual/augmentedTypesModule2_test.go
@@ -9,7 +9,6 @@ import (
 )
 
 func TestAugmentedTypesModule2(t *testing.T) {
-
 	t.Parallel()
 	defer testutil.RecoverAndFail(t, "Panic on fourslash test")
 	const content = `function /*11*/m2f(x: number) { };
