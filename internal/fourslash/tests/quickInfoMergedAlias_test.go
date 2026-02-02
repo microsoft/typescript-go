4a89dca84f9d61952cd400f065057c657299280b
Anders Hejlsberg <andersh@microsoft.com>
2026-02-02 07:29:43 -1000

    Fix formatting

--- internal/fourslash/tests/quickInfoMergedAlias_test.go
+++ internal/fourslash/tests/quickInfoMergedAlias_test.go
@@ -43,33 +43,4 @@ let x2: foo/*11*/.bar;`
 	f, done := fourslash.NewFourslash(t, nil /*capabilities*/, content)
 	defer done()
 	f.VerifyBaselineHover(t)
-// 	const content = `// @Filename: /a.ts
-// /**
-//  * A function
-//  */
-// export function foo/*1*/() {}
-// // @Filename: /b.ts
-// import { foo/*2*/ } from './a';
-// export { foo/*3*/ }
-
-// /**
-//  * A type
-//  */
-// type foo/*4*/ = number;
-
-// foo/*5*/()
-// let x1: foo/*6*/;
-// // @Filename: /c.ts
-// import { foo/*7*/ } from './b';
-
-// /**
-//  * A namespace
-//  */
-// namespace foo/*8*/ {
-//     export type bar = string[];
-// }
-
-// foo/*9*/()
-// let x1: foo/*10*/;
-// let x2: foo/*11*/.bar;`
 }
