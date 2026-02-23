7e0a20324c39954cf7c1f20521c0f065e7746f70
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2026-02-23 15:49:30 -0800

    Fix flags

--- internal/transformers/estransforms/async.go
+++ internal/transformers/estransforms/async.go
@@ -12,7 +12,6 @@ import (
 type asyncContextFlags int
 
 const (
-	asyncContextNone        asyncContextFlags = 0
 	asyncContextNonTopLevel asyncContextFlags = 1 << iota
 	asyncContextHasLexicalThis
 )
