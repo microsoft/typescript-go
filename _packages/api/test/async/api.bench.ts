a434c177d26e1c4742a15204fbc1ecaeaf6cfc43
Andrew Branch <andrew@wheream.io>
2026-02-25 20:30:59 -0800

    Revert debugging change

--- _packages/api/test/async/api.bench.ts
+++ _packages/api/test/async/api.bench.ts
@@ -63,21 +63,20 @@ export async function runBenchmarks(options?: { filter?: string; singleIteration
     let file: SourceFile;
     let tsFile: ts.SourceFile;
 
-    // const programIdentifierCount = await (async () => {
-    //     await spawnAPI();
-    //     await loadSnapshot();
-    //     await getProgramTS();
-    //     let count = 0;
-    //     file!.forEachChild(function visit(node) {
-    //         if (node.kind === SyntaxKind.Identifier) {
-    //             count++;
-    //         }
-    //         node.forEachChild(visit);
-    //     });
-    //     await teardown();
-    //     return count;
-    // })();
-    const programIdentifierCount = 8000;
+    const programIdentifierCount = await (async () => {
+        await spawnAPI();
+        await loadSnapshot();
+        await getProgramTS();
+        let count = 0;
+        file!.forEachChild(function visit(node) {
+            if (node.kind === SyntaxKind.Identifier) {
+                count++;
+            }
+            node.forEachChild(visit);
+        });
+        await teardown();
+        return count;
+    })();
 
     // Tinybench's `isFnAsyncResource` probes each task function by *calling*
     // it once during `.add()` to detect whether it returns a Promise.
