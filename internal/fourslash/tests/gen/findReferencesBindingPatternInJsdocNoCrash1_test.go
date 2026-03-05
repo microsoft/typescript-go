51f64391c6ed45f5d1b8b56b0c04b8988309b2ab
Mateusz Burzyński <mateuszburzynski@gmail.com>
2026-03-05 22:58:53 +0100

    Fixed a find reference crash related to JSDoc function types in TS files

--- internal/fourslash/tests/gen/findReferencesBindingPatternInJsdocNoCrash1_test.go
+++ internal/fourslash/tests/gen/findReferencesBindingPatternInJsdocNoCrash1_test.go
@@ -11,7 +11,6 @@ import (
 )
 
 func TestFindReferencesBindingPatternInJsdocNoCrash1(t *testing.T) {
-	fourslash.SkipIfFailing(t)
 	t.Parallel()
 	defer testutil.RecoverAndFail(t, "Panic on fourslash test")
 	const content = `// @moduleResolution: bundler
