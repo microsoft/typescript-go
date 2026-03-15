4b164c6e4c8576330ed2353c566a2d7c1a949bac
Mateusz Burzyński <mateuszburzynski@gmail.com>
2026-03-15 21:04:40 +0100

    skipToEndOf to on grammar errors to avoid bogus tail tokenization

--- internal/fourslash/tests/gen/formattingSkippedTokens_test.go
+++ internal/fourslash/tests/gen/formattingSkippedTokens_test.go
@@ -28,11 +28,11 @@ function a(
 	f.GoToMarker(t, "1")
 	f.VerifyCurrentLineContent(t, `foo(): Bar { }`)
 	f.GoToMarker(t, "2")
-	f.VerifyCurrentLineContent(t, `function Foo() #   { }`)
+	f.VerifyCurrentLineContent(t, `function Foo      () #   { }`)
 	f.GoToMarker(t, "3")
 	f.VerifyCurrentLineContent(t, `4 +: 5`)
 	f.GoToMarker(t, "4")
 	f.VerifyCurrentLineContent(t, `    : T) { }`)
 	f.GoToMarker(t, "5")
-	f.VerifyCurrentLineContent(t, `var x =`)
+	f.VerifyCurrentLineContent(t, `var x       =`)
 }
