6bbe8938dc8a0fa8ad8ba87be2bacb2e458c08f9
Mateusz Burzyński <mateuszburzynski@gmail.com>
2026-03-15 20:33:33 +0100

    add test case

---
+++ internal/fourslash/tests/formatTemplateLiteralInMalformedPropertySignatureNoCrash1_test.go
@@ -0,0 +1,31 @@
+package fourslash_test
+
+import (
+	"testing"
+
+	"github.com/microsoft/typescript-go/internal/fourslash"
+	"github.com/microsoft/typescript-go/internal/testutil"
+)
+
+func TestFormatTemplateLiteralInMalformedPropertySignatureNoCrash1(t *testing.T) {
+	t.Parallel()
+	defer testutil.RecoverAndFail(t, "Panic on fourslash test")
+	content := `// @Filename: /resource-card.tsx
+interface Props {
+  iconOnly?: boolean
+
+
+const ResourceCard: React.FC<Props> = (props) => {
+  return (
+    <IoLayersOutline
+      className={` + "`" + `${match ? 'text-primary-foreground' : 'text-foreground'}` + "`" + `}
+    />
+  )
+}
+
+export default ResourceCard
+`
+	f, done := fourslash.NewFourslash(t, nil /*capabilities*/, content)
+	defer done()
+	f.FormatDocument(t, "")
+}
