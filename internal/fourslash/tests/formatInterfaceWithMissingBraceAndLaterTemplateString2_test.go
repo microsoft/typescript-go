904f38a93700aee0c77054f9a9fc7ee5e6d112be
Mateusz Burzyński <mateuszburzynski@gmail.com>
2026-03-17 13:23:59 +0100

    reformat test

--- internal/fourslash/tests/formatInterfaceWithMissingBraceAndLaterTemplateString2_test.go
+++ internal/fourslash/tests/formatInterfaceWithMissingBraceAndLaterTemplateString2_test.go
@@ -10,9 +10,31 @@ import (
 func TestFormatInterfaceWithMissingBraceAndLaterTemplateString2(t *testing.T) {
 	t.Parallel()
 	defer testutil.RecoverAndFail(t, "Panic on fourslash test")
-	const content = "// @Filename: /FormCheck.tsx\ninterface FormCheckProps {\n\nconst FormCheck: DynamicRefForwardingComponent<'input', FormCheckProps> =\n  React.forwardRef(\n    () => {\n      return <div className={`${bsPrefix}-reverse`} />;\n    },\n  );\n\nFormCheck.displayName = 'FormCheck';\n"
+	const content = `
+// @Filename: /FormCheck.tsx
+interface FormCheckProps {
+
+const FormCheck: DynamicRefForwardingComponent<'input', FormCheckProps> =
+  React.forwardRef(
+	    () => {
+	      return <div className={` + "`" + `${bsPrefix}-reverse` + "`" + `} />;
+    },
+  );
+
+FormCheck.displayName = 'FormCheck';
+`
 	f, done := fourslash.NewFourslash(t, nil /*capabilities*/, content)
 	defer done()
 	f.FormatDocument(t, "")
-	f.VerifyCurrentFileContent(t, "interface FormCheckProps {\n\nconst FormCheck: DynamicRefForwardingComponent<'input', FormCheckProps> =\n    React.forwardRef(\n        () => {\n            return <div className={`${bsPrefix}-reverse`} />;\n        },\n    );\n\nFormCheck.displayName = 'FormCheck';\n")
+	f.VerifyCurrentFileContent(t, `interface FormCheckProps {
+
+const FormCheck: DynamicRefForwardingComponent<'input', FormCheckProps> =
+    React.forwardRef(
+        () => {
+            return <div className={` + "`" + `${bsPrefix}-reverse` + "`" + `} />;
+        },
+    );
+
+FormCheck.displayName = 'FormCheck';
+`)
 }
