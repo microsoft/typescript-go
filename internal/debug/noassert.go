6ca7f7559f48e95d415d55ba9a1eb11b415e4ff2
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2026-03-17 17:24:01 -0700

    fmt

--- internal/debug/noassert.go
+++ internal/debug/noassert.go
@@ -7,16 +7,16 @@ import (
 	"fmt"
 )
 
-func Assert[T comparable](value T, message ...any)                                                {}
-func AssertZero[T comparable](value T, message ...any)                                            {}
-func AssertEqual(a fmt.Stringer, b fmt.Stringer, msg ...any)                                      {}
-func AssertLessThan[T cmp.Ordered](a T, b T, message ...any)                                     {}
-func AssertLessThanOrEqual[T cmp.Ordered](a T, b T, message ...any)                              {}
-func AssertGreaterThan[T cmp.Ordered](a T, b T, message ...any)                                  {}
-func AssertGreaterThanOrEqual[T cmp.Ordered](a T, b T, message ...any)                           {}
-func CheckNonZero[T comparable](value T, message ...any) T                                        { return value }
-func AssertEach[TElem any](value []TElem, test func(TElem) bool, message ...any)                  {}
-func CheckEach[TElem any](value []TElem, test func(TElem) bool, message ...any) []TElem           { return value }
+func Assert[T comparable](value T, message ...any)                                              {}
+func AssertZero[T comparable](value T, message ...any)                                          {}
+func AssertEqual(a fmt.Stringer, b fmt.Stringer, msg ...any)                                    {}
+func AssertLessThan[T cmp.Ordered](a T, b T, message ...any)                                    {}
+func AssertLessThanOrEqual[T cmp.Ordered](a T, b T, message ...any)                             {}
+func AssertGreaterThan[T cmp.Ordered](a T, b T, message ...any)                                 {}
+func AssertGreaterThanOrEqual[T cmp.Ordered](a T, b T, message ...any)                          {}
+func CheckNonZero[T comparable](value T, message ...any) T                                      { return value }
+func AssertEach[TElem any](value []TElem, test func(TElem) bool, message ...any)                {}
+func CheckEach[TElem any](value []TElem, test func(TElem) bool, message ...any) []TElem         { return value }
 func AssertEachNode[TElem NodeLike](nodes []TElem, test func(elem TElem) bool, message ...any)  {}
 func AssertNode[TElem NodeLike](node TElem, test func(elem TElem) bool, message ...any)         {}
 func AssertNotNode[TElem NodeLike](node TElem, test func(elem TElem) bool, message ...any)      {}
