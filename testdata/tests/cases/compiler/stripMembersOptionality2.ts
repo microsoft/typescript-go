882fb562c5d18878e3d8d795e8ab4d7a5fb588dc
Mateusz Burzyński <mateuszburzynski@gmail.com>
2026-03-30 09:18:41 +0200

    add extra test

--- testdata/tests/cases/compiler/stripMembersOptionality2.ts
+++ testdata/tests/cases/compiler/stripMembersOptionality2.ts
@@ -8,3 +8,10 @@ type WithObject1 = Required<{ a?: string | undefined }>;
 const obj1: WithObject1 = { a: undefined };
 type WithArray1 = Required<[(string | undefined)?]>;
 const tup1: WithArray1 = [undefined];
+
+type ToStringOrUnd<T> = { [P in keyof T]-?: string | undefined };
+
+type WithObject2 = ToStringOrUnd<{ a: 1; b?: 2 }>;
+const obj2: WithObject2 = { a: "1", b: undefined };
+type WithArray2 = ToStringOrUnd<[1, 2?]>;
+const tup2: WithArray2 = ["1", undefined];
