868eeb3561896023b1d1d24267cf6067c73bf59f
John Favret <64748847+johnfav03@users.noreply.github.com>
2026-04-23 12:23:46 -0500

    moved test to tspath

--- internal/project/watch_test.go
+++ internal/project/watch_test.go
@@ -3,22 +3,8 @@ package project
 import (
 	"testing"
 
-	"github.com/microsoft/typescript-go/internal/tspath"
 	"gotest.tools/v3/assert"
 )
-
-func TestGetPathComponentsForWatching(t *testing.T) {
-	t.Parallel()
-
-	assert.DeepEqual(t, tspath.GetPathComponentsForWatching("/project", ""), []string{"/", "project"})
-	assert.DeepEqual(t, tspath.GetPathComponentsForWatching("C:\\project", ""), []string{"C:/", "project"})
-	assert.DeepEqual(t, tspath.GetPathComponentsForWatching("//server/share/project/tsconfig.json", ""), []string{"//server/share", "project", "tsconfig.json"})
-	assert.DeepEqual(t, tspath.GetPathComponentsForWatching(`\\server\share\project\tsconfig.json`, ""), []string{"//server/share", "project", "tsconfig.json"})
-	assert.DeepEqual(t, tspath.GetPathComponentsForWatching("C:\\Users", ""), []string{"C:/Users"})
-	assert.DeepEqual(t, tspath.GetPathComponentsForWatching("C:\\Users\\andrew\\project", ""), []string{"C:/Users/andrew", "project"})
-	assert.DeepEqual(t, tspath.GetPathComponentsForWatching("/home", ""), []string{"/home"})
-	assert.DeepEqual(t, tspath.GetPathComponentsForWatching("/home/andrew/project", ""), []string{"/home/andrew", "project"})
-}
 
 func TestNilWatchedFilesClone(t *testing.T) {
 	t.Parallel()
