b8b15db46efaf27c341f569d2d2691a27cb10911
Ryan Cavanaugh <RyanCavanaugh@users.noreply.github.com>
2026-04-28 15:15:41 -0700

    Apply suggestion from @RyanCavanaugh

--- internal/core/stacklimit.go
+++ internal/core/stacklimit.go
@@ -10,7 +10,7 @@ import (
 // positive integer, applies it via runtime/debug.SetMaxStack. Useful for
 // catching runaway recursion. Should be called from program entry points.
 func ApplyDebugStackLimit() {
-	v := os.Getenv("TSC_DEBUG_STACK_LIMIT") //nolint:forbidigo
+	v := os.Getenv("TSGO_DEBUG_STACK_LIMIT") //nolint:forbidigo
 	if v == "" {
 		return
 	}
