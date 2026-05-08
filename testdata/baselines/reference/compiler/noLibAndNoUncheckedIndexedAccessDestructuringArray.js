6e0707d1a392924d0bc737f3ba0d40903612fe1a
Daniel Rosenwasser <DanielRosenwasser@users.noreply.github.com>
2026-05-08 01:45:58 +0000

    Accepted baselines.

--- testdata/baselines/reference/compiler/noLibAndNoUncheckedIndexedAccessDestructuringArray.js
+++ testdata/baselines/reference/compiler/noLibAndNoUncheckedIndexedAccessDestructuringArray.js
@@ -1,10 +1,24 @@
 //// [tests/cases/compiler/noLibAndNoUncheckedIndexedAccessDestructuringArray.ts] ////
 
-//// [noLibAndNoUncheckedIndexedAccessDestructuringArray.ts]
+//// [globals.ts]
+interface Array<T> {}
+interface Boolean {}
+interface Function {}
+interface CallableFunction {}
+interface NewableFunction {}
+interface IArguments {}
+interface Number {}
+interface Object {}
+interface RegExp {}
+interface String {}
+
+//// [input.ts]
 declare var x: string[];
 var [a] = x;
 
 
-//// [noLibAndNoUncheckedIndexedAccessDestructuringArray.js]
+//// [globals.js]
+"use strict";
+//// [input.js]
 "use strict";
 var [a] = x;
