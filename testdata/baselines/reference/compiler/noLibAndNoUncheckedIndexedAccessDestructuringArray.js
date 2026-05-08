529add1b7ede233b8884cd55ff42701dd3e0e320
Daniel Rosenwasser <DanielRosenwasser@users.noreply.github.com>
2026-05-08 01:42:33 +0000

    Accepted baselines.

---
+++ testdata/baselines/reference/compiler/noLibAndNoUncheckedIndexedAccessDestructuringArray.js
@@ -0,0 +1,10 @@
+//// [tests/cases/compiler/noLibAndNoUncheckedIndexedAccessDestructuringArray.ts] ////
+
+//// [noLibAndNoUncheckedIndexedAccessDestructuringArray.ts]
+declare var x: string[];
+var [a] = x;
+
+
+//// [noLibAndNoUncheckedIndexedAccessDestructuringArray.js]
+"use strict";
+var [a] = x;
