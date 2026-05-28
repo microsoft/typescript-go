6f2746518c79ebf7458d03b48ec4ff80ca179fa2
Isabel Duan <isabelduan@microsoft.com>
2026-05-28 15:30:00 -0700

    more loc

--- _extension/src/statusBar.ts
+++ _extension/src/statusBar.ts
@@ -3,9 +3,9 @@ import { jsTsLanguageModes } from "./util";
 
 export function setupStatusBar(version: string): vscode.Disposable {
     const statusItem = vscode.languages.createLanguageStatusItem("typescript.native-preview.status", jsTsLanguageModes);
-    statusItem.name = "TypeScript Native Preview";
+    statusItem.name = vscode.l10n.t("TypeScript Native Preview");
     statusItem.text = vscode.l10n.t(`$(beaker) tsgo {0}`, version);
-    statusItem.detail = "TypeScript Native Preview Language Server";
+    statusItem.detail = vscode.l10n.t("TypeScript Native Preview Language Server");
     statusItem.command = {
         title: vscode.l10n.t("Show Menu"),
         command: "typescript.native-preview.showMenu",
