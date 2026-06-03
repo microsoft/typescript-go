62fe1257075bfea7cc13dcdc431b161abd6ad350
Anders Hejlsberg <andersh@microsoft.com>
2026-06-03 11:46:46 -0700

    Add tests

---
+++ testdata/baselines/reference/compiler/disallowUnerasableAssertion.js
@@ -0,0 +1,13 @@
+//// [tests/cases/compiler/disallowUnerasableAssertion.ts] ////
+
+//// [disallowUnerasableAssertion.ts]
+export const c1 = 1 + 1 as number / 2;
+export const c2 = (1 + 1 as number) / 2;
+export const c3 = 1 + 1 as number === 2;
+
+
+//// [disallowUnerasableAssertion.js]
+export const c1 = 1 + 1;
+/ 2;
+export const c2 = (1 + 1) / 2;
+export const c3 = 1 + 1 === 2;
---
+++ testdata/tests/cases/compiler/disallowUnerasableAssertion.ts
@@ -0,0 +1,3 @@
+export const c1 = 1 + 1 as number / 2;
+export const c2 = (1 + 1 as number) / 2;
+export const c3 = 1 + 1 as number === 2;
