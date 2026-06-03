f40297d58e61cdba3d3d252c9daa8841d2a3ebee
Anders Hejlsberg <andersh@microsoft.com>
2026-06-03 12:20:38 -0700

    Add more tests

--- testdata/baselines/reference/compiler/disallowUnerasableAssertion.js
+++ testdata/baselines/reference/compiler/disallowUnerasableAssertion.js
@@ -4,6 +4,10 @@
 export const c1 = 1 + 1 as number / 2;
 export const c2 = (1 + 1 as number) / 2;
 export const c3 = 1 + 1 as number === 2;
+export const c4 = 1 + 1 as number > 2;
+export const c5 = 1 + 1 as number >= 2;
+export const c6 = 1 + 1 as number >> 2;
+export const c7 = 1 + 1 as number << 2;
 
 
 //// [disallowUnerasableAssertion.js]
@@ -11,3 +15,9 @@ export const c1 = 1 + 1;
 / 2;
 export const c2 = (1 + 1) / 2;
 export const c3 = 1 + 1 === 2;
+export const c4 = 1 + 1 > 2;
+export const c5 = 1 + 1 >= 2;
+export const c6 = 1 + 1;
+ >> 2;
+export const c7 = 1 + 1;
+ << 2;
--- testdata/tests/cases/compiler/disallowUnerasableAssertion.ts
+++ testdata/tests/cases/compiler/disallowUnerasableAssertion.ts
@@ -1,3 +1,7 @@
 export const c1 = 1 + 1 as number / 2;
 export const c2 = (1 + 1 as number) / 2;
 export const c3 = 1 + 1 as number === 2;
+export const c4 = 1 + 1 as number > 2;
+export const c5 = 1 + 1 as number >= 2;
+export const c6 = 1 + 1 as number >> 2;
+export const c7 = 1 + 1 as number << 2;
