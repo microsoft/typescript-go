8e4be4041176a471ac05ef898173dd15b44074bc
Daniel Rosenwasser <DanielRosenwasser@users.noreply.github.com>
2026-06-05 12:14:51 -0700

    Don't create `LanguageService`s without programs.

--- internal/ls/file_rename.go
+++ internal/ls/file_rename.go
@@ -26,9 +26,6 @@ type toImport struct {
 
 func (l *LanguageService) GetEditsForFileRename(ctx context.Context, oldURI lsproto.DocumentUri, newURI lsproto.DocumentUri) []lsproto.TextDocumentEditOrCreateFileOrRenameFileOrDeleteFile {
 	program := l.GetProgram()
-	if program == nil {
-		return nil
-	}
 	oldPath := oldURI.FileName()
 	newPath := newURI.FileName()
 
