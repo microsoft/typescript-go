6ccba7f62176f291f1f410f5514203faad829a35
Wesley Wigham <wewigham@microsoft.com>
2026-06-11 13:30:59 -0700

    Enable node reuse for binary expressions with non-local type declarations

--- testdata/baselines/reference/compiler/cjsExportsDuplication.js
+++ testdata/baselines/reference/compiler/cjsExportsDuplication.js
@@ -5,7 +5,16 @@ exports.foo = 42
 exports.foo = "hello"
 exports.foo = true
 
+//// [file2.js]
+exports.foo = 42
+/** @type {string} */
+exports.foo = "hello"
+/** @type {boolean} */
+exports.foo = true
+
 
 
 //// [file.d.ts]
 export declare var foo: "hello" | 42 | true;
+//// [file2.d.ts]
+export declare var foo: string;
--- testdata/tests/cases/compiler/cjsExportsDuplication.ts
+++ testdata/tests/cases/compiler/cjsExportsDuplication.ts
@@ -6,3 +6,10 @@
 exports.foo = 42
 exports.foo = "hello"
 exports.foo = true
+
+// @filename: file2.js
+exports.foo = 42
+/** @type {string} */
+exports.foo = "hello"
+/** @type {boolean} */
+exports.foo = true
