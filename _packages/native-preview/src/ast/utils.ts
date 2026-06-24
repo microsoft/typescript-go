8a4a4d349f8abe8b10ad53162f15e0f8017a8b42
Andrew Branch <andrew@wheream.io>
2026-06-24 13:49:52 -0700

    Format

--- _packages/native-preview/src/ast/utils.ts
+++ _packages/native-preview/src/ast/utils.ts
@@ -47,6 +47,6 @@ export function cloneSourceFileData(sourceFile: SourceFile): Record<string, unkn
         moduleAugmentations: sourceFile.moduleAugmentations,
         ambientModuleNames: sourceFile.ambientModuleNames,
         externalModuleIndicator: sourceFile.externalModuleIndicator,
-        tokenCache: undefined,
+        tokenCache: undefined,
     };
 }
