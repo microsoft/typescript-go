e7fa528ae9c309cb7c4e6b7e59eed48a70f2fe66
Wesley Wigham <wewigham@microsoft.com>
2026-08-13 13:20:39 -0700

    Parse private names in type queries, forbid in declaration emit

--- internal/printer/printer.go
+++ internal/printer/printer.go
@@ -1196,7 +1196,7 @@ func (p *Printer) emitQualifiedName(node *ast.QualifiedName) {
 	state := p.enterNode(node.AsNode())
 	p.emitEntityName(node.Left)
 	p.writePunctuation(".")
-	p.emitIdentifierName(node.Right.AsIdentifier())
+	p.emitPropertyName(node.Right)
 	p.exitNode(node.AsNode(), state)
 }
 
