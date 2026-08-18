5e2af9b5157460a939d611acbc693fe1b369c9c8
Anders Hejlsberg <andersh@microsoft.com>
2026-08-18 08:18:06 -0700

    Remove "excessive stack depth" relation error

--- internal/checker/relater.go
+++ internal/checker/relater.go
@@ -70,9 +70,8 @@ const (
 	RelationComparisonResultReportsUnmeasurable RelationComparisonResult = 1 << 3
 	RelationComparisonResultReportsUnreliable   RelationComparisonResult = 1 << 4
 	RelationComparisonResultComplexityOverflow  RelationComparisonResult = 1 << 5
-	RelationComparisonResultStackDepthOverflow  RelationComparisonResult = 1 << 6
 	RelationComparisonResultReportsMask                                  = RelationComparisonResultReportsUnmeasurable | RelationComparisonResultReportsUnreliable
-	RelationComparisonResultOverflow                                     = RelationComparisonResultComplexityOverflow | RelationComparisonResultStackDepthOverflow
+	RelationComparisonResultOverflow                                     = RelationComparisonResultComplexityOverflow
 )
 
 type DiagnosticAndArguments struct {
@@ -372,15 +371,14 @@ func (c *Checker) checkTypeRelatedToEx(
 	if r.overflow {
 		// Record this relation as having failed such that we don't attempt the overflowing operation again.
 		id, _ := getRelationKey(source, target, IntersectionStateNone, relation == c.identityRelation, false /*ignoreConstraints*/)
-		relation.set(id, RelationComparisonResultFailed|core.IfElse(r.relationCount <= 0, RelationComparisonResultComplexityOverflow, RelationComparisonResultStackDepthOverflow))
+		relation.set(id, RelationComparisonResultFailed|RelationComparisonResultComplexityOverflow)
 		if tr := c.tracer; tr != nil {
 			tr.Instant(tracing.PhaseCheckTypes, "checkTypeRelatedTo_DepthLimit", map[string]any{"sourceId": source.id, "targetId": target.id, "depth": len(r.sourceStack), "targetDepth": len(r.targetStack)})
 		}
-		message := core.IfElse(r.relationCount <= 0, diagnostics.Excessive_complexity_comparing_types_0_and_1, diagnostics.Excessive_stack_depth_comparing_types_0_and_1)
 		if errorNode == nil {
 			errorNode = c.currentNode
 		}
-		c.reportDiagnostic(NewDiagnosticForNode(errorNode, message, c.TypeToString(source), c.TypeToString(target)), diagnosticOutput)
+		c.reportDiagnostic(NewDiagnosticForNode(errorNode, diagnostics.Excessive_complexity_comparing_types_0_and_1, c.TypeToString(source), c.TypeToString(target)), diagnosticOutput)
 	} else if r.errorChain != nil {
 		// Check if we should issue an extra diagnostic to produce a quickfix for a slightly incorrect import statement
 		if headMessage != nil && errorNode != nil && result == TernaryFalse && source.symbol != nil && c.exportTypeLinks.Has(source.symbol) {
@@ -3107,10 +3105,7 @@ func (r *Relater) recursiveTypeRelatedTo(source *Type, target *Type, reportError
 		} else {
 			r.c.reliabilityFlags |= entry & (RelationComparisonResultReportsUnmeasurable | RelationComparisonResultReportsUnreliable)
 			if reportErrors && entry&RelationComparisonResultOverflow != 0 {
-				message := core.IfElse(entry&RelationComparisonResultComplexityOverflow != 0,
-					diagnostics.Excessive_complexity_comparing_types_0_and_1,
-					diagnostics.Excessive_stack_depth_comparing_types_0_and_1)
-				r.reportError(message, r.c.TypeToString(source), r.c.TypeToString(target))
+				r.reportError(diagnostics.Excessive_complexity_comparing_types_0_and_1, r.c.TypeToString(source), r.c.TypeToString(target))
 			}
 			if entry&RelationComparisonResultSucceeded != 0 {
 				return TernaryTrue
@@ -3136,8 +3131,10 @@ func (r *Relater) recursiveTypeRelatedTo(source *Type, target *Type, reportError
 		}
 	}
 	if len(r.sourceStack) == 100 || len(r.targetStack) == 100 {
-		r.overflow = true
-		return TernaryFalse
+		// We stop relating if we reach 100 levels of nesting. This is a backstop to catch infinite recursion
+		// that wasn't caught by isDeeplyNestedType. It will also stop relating types that truly are over 100
+		// levels deep, but those are exceedingly rare.
+		return TernaryMaybe
 	}
 	maybeStart := len(r.maybeKeys)
 	r.maybeKeys = append(r.maybeKeys, id)
@@ -4841,7 +4838,6 @@ func (r *Relater) reportRelationError(message *diagnostics.Message, source *Type
 	// Suppress if next message is an excessive complexity/stack depth message for source and target or a readonly
 	// vs. mutable error for source and target
 	case diagnostics.Excessive_complexity_comparing_types_0_and_1,
-		diagnostics.Excessive_stack_depth_comparing_types_0_and_1,
 		diagnostics.The_type_0_is_readonly_and_cannot_be_assigned_to_the_mutable_type_1:
 		if r.chainArgsMatch(generalizedSourceType, targetType) {
 			return
